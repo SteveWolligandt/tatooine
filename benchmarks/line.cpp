48b6a56fc1d9e8db01330e6eb4a6f7af0ecd1451
Steve Wolligandt <steve@isg.cs.uni-magdeburg.de>
2020-02-04 14:56:28 +0100

    using hermite with polynomials

--- benchmarks/line.cpp
+++ benchmarks/line.cpp
@@ -5,10 +5,9 @@
 namespace tatooine::benchmark {
 //==============================================================================
 TATBENCH(line_sampling) {
-  vec v{1.0, 2.0};
   parameterized_line<double, 2> l{
       {vec{0.0, 0.0}, 0.0}, {vec{1.0, 1.0}, 1.0}, {vec{2.0, 0.0}, 2.0}};
-  linspace ts(0.0, 2.0, 1000);
+  linspace ts(0.0, 2.0, 10000);
   for (auto _ : state) { l.resample(ts); }
 }
 // Register the function as a benchmark
