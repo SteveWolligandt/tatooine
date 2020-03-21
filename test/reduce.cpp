5d9f3344b9dd042e30f0697261b404984f7abd0d
Steve Wolligandt <stevewolligandt@gmail.com>
2020-03-21 11:59:58 +0100

    random stuff

--- test/reduce.cpp
+++ test/reduce.cpp
@@ -1,19 +1,14 @@
 #include <tatooine/gpu/reduce.h>
 #include <tatooine/random.h>
 
-#include <boost/range/algorithm/generate.hpp>
 #include <boost/range/numeric.hpp>
 #include <catch2/catch.hpp>
 //==============================================================================
 namespace tatooine::gpu::test {
 //==============================================================================
 TEST_CASE("reduce0", "[gpu][reduce][texture]") {
-  std::mt19937 rand_eng{std::random_device{}()};
-  random_uniform<float, std::mt19937> rand{rand_eng};
-  const size_t                   width = 1024, height = 1024;
-
-  std::vector<float> rand_data(width * height);
-  boost::generate(rand_data, [&] { return rand(); });
+  constexpr size_t      width = 1024, height = 1024;
+  auto            rand_data = random_uniform_vector<float>(width * height);
   yavin::tex2r32f data{rand_data, width, height};
 
   auto reduced = reduce(data);
