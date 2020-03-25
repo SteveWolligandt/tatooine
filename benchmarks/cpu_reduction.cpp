7ec9394a75c85f585b618c3c2b412b3304fd0bee
Steve Wolligandt <stevewolligandt@gmail.com>
2020-03-25 13:31:42 +0100

    reduction benchmarks

--- benchmarks/cpu_reduction.cpp
+++ benchmarks/cpu_reduction.cpp
@@ -1,11 +1,13 @@
-#include "benchmark.h"
-
-#include <tatooine/gpu/reduce.h>
 #include <tatooine/random.h>
+#include <yavin/texture.h>
+#include <execution>
+#include <numeric>
+
+#include "benchmark.h"
 //==============================================================================
 namespace tatooine::benchmark {
 //==============================================================================
-static void texture_reduction(::benchmark::State& state) {
+static void cpu_reduction(::benchmark::State& state) {
   auto         seed   = std::random_device{}();
   std::mt19937 eng{seed};
 
@@ -13,21 +15,21 @@ static void texture_reduction(::benchmark::State& state) {
   for (auto _ : state) {
     state.PauseTiming();  // Stop timers. They will not count until they are
                           // resumed.
-    const size_t             width  = state.range(2);
-    const size_t             height = state.range(3);
+    const size_t             width = state.range(0);
+    const size_t             height = state.range(1);
     const std::vector<float> rand_data =
         random_uniform_vector<float>(width * height, 0.0f, 1.0f, eng);
     const yavin::tex2r32f data_tex{rand_data, width, height};
     state.ResumeTiming();  // And resume timers. They are now counting again.
-    gpu::reduce(data_tex, state.range(0), state.range(1));
+    auto downloaded_data = data_tex.download_data();
+    std::reduce(std::execution::par, begin(downloaded_data),
+                end(downloaded_data), 0.0f);
   }
 }
-static void texture_reduction_args(::benchmark::internal::Benchmark* b) {
-  for (int res = 32; res <= 1024; res *= 2) {
-    for (int w = 8; w <= 32; w *= 2) { b->Args({w, w, res, res}); }
-  }
+static void cpu_reduction_args(::benchmark::internal::Benchmark* b) {
+  for (int i = 32; i <= 1024; i *= 2) b->Args({i,i});
 }
-BENCHMARK(texture_reduction)->Apply(texture_reduction_args);
+BENCHMARK(cpu_reduction)->Apply(cpu_reduction_args);
 //==============================================================================
 }  // namespace tatooine::benchmark
 //==============================================================================
