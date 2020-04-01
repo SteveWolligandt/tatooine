e958351b4136657e93ec7e50fce60ff0584820a8
Steve Wolligandt <steve@isg.cs.ovgu.de>
2020-04-01 16:16:03 +0200

    loading indicator

--- steadification/steadification.h
+++ steadification/steadification.h
@@ -471,8 +471,10 @@ class steadification {
         std::cerr << " " << int(progress * 100.0) << " %\r";
         std::this_thread::sleep_for(std::chrono::milliseconds{100});
       }
-      std::cerr << "integrating pathsurfaces ... done!                       "
-                   "            \n";
+        std::cerr << "integrating pathsurfaces ... ";
+        int pos = barWidth * progress;
+        for (int i = 0; i < barWidth; ++i) { std::cerr << "\u2588"; }
+        std::cerr << " 100 %\n";
     }};
 
 #if defined(TATOOINE_STEADIFICATION_PARALLEL)
