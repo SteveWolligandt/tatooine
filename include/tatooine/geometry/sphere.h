8c46d8819682510037c56aa33bb27af296e9e6e1
Steve Wolligandt <stevewolligandt@gmail.com>
2020-05-12 15:59:32 +0200

    general sphere as geometry

--- include/tatooine/geometry/sphere.h
+++ include/tatooine/geometry/sphere.h
@@ -1,53 +1,44 @@
-#ifndef TATOOINE_GEOMETRY_CIRCLE_H
-#define TATOOINE_GEOMETRY_CIRCLE_H
-
+#ifndef TATOOINE_GEOMETRY_SPHERE_H
+#define TATOOINE_GEOMETRY_SPHERE_H
+//==============================================================================
 #include "../tensor.h"
 #include "primitive.h"
-
 //==============================================================================
 namespace tatooine::geometry {
 //==============================================================================
-
-template <typename Real>
-struct circle : primitive<Real, 2> {
-  using this_t   = circle<Real>;
-  using parent_t = primitive<Real, 2>;
+template <typename Real, size_t N>
+struct sphere : primitive<Real, N> {
+  using this_t   = sphere<Real, N>;
+  using parent_t = primitive<Real, N>;
   using typename parent_t::pos_t;
-
   //============================================================================
  private:
   Real  m_radius;
   pos_t m_center;
-
   //============================================================================
  public:
-  circle(Real radius, pos_t&& center)
+  sphere(Real radius, pos_t&& center)
       : m_radius{radius}, m_center{std::move(center)} {}
-  circle(Real radius, const pos_t& center)
+  sphere(Real radius, const pos_t& center)
       : m_radius{radius}, m_center{center} {}
-
   //----------------------------------------------------------------------------
-  circle(const circle&) = default;
-  circle(circle&&) = default;
-  circle& operator=(const circle&) = default;
-  circle& operator=(circle&&) = default;
-
+  sphere(const sphere&) = default;
+  sphere(sphere&&) = default;
+  sphere& operator=(const sphere&) = default;
+  sphere& operator=(sphere&&) = default;
   //============================================================================
   bool is_inside(const pos_t& x) const override {
-    return distance(center, x) <= radius;
+    return distance(center, x) <= m_radius;
   }
-
   //----------------------------------------------------------------------------
   auto  radius() const { return m_radius; }
   auto& radius() { return m_radius; }
-
   //----------------------------------------------------------------------------
   const auto& center() const { return m_center; }
   auto&       center() { return m_center; }
 };
-
 //==============================================================================
 }  // namespace tatooine::geometry
 //==============================================================================
-
 #endif
+
