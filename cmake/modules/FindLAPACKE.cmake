590c79eb5fc82c0ae5e82392d87d96b3b49ba445
Steve Wolligandt <stevewolligandt@gmail.com>
2020-05-14 22:01:01 +0200

    cmake finding lapacke and ginac

--- cmake/modules/FindLAPACKE.cmake
+++ cmake/modules/FindLAPACKE.cmake
@@ -1,249 +1,173 @@
-#.rst:
-# FindLAPACKE
-# -------------
+# - Find the LAPACKE library
 #
-# Find LAPACKE include dirs and libraries
+# Usage:
+#   FIND_PACKAGE(LAPACKE [REQUIRED] [QUIET] )
 #
-# Use this module by invoking find_package with the form::
-#
-#   find_package(LAPACKE
-#     [REQUIRED]             # Fail with error if LAPACKE is not found
-#     [COMPONENTS <libs>...] # List of libraries to look for
-#     )
-#
-# Valid names for COMPONENTS libraries are::
-#
-#   ALL                      - Find all libraries
-#   LAPACKE_H                - Find the lapacke.h header file
-#   LAPACKE                  - Find a LAPACKE library
-#   LAPACK                   - Find a LAPACK library
-#   CBLAS                    - Find a CBLAS library
-#   BLAS                     - Find a BLAS library
-#
-#  Not specifying COMPONENTS is identical to choosing ALL
-#
-# This module defines::
-#
-#   LAPACKE_FOUND            - True if headers and requested libraries were found
-#   LAPACKE_INCLUDE_DIRS     - LAPACKE include directories
-#   LAPACKE_LIBRARIES        - LAPACKE component libraries to be linked
-#
-#
-# This module reads hints about search locations from variables
-# (either CMake variables or environment variables)::
-#
-#   LAPACKE_ROOT             - Preferred installation prefix for LAPACKE
-#   LAPACKE_DIR              - Preferred installation prefix for LAPACKE
-#
-#
-# The following :prop_tgt:`IMPORTED` targets are also defined::
-#
-#   LAPACKE::LAPACKE         - Imported target for the LAPACKE library
-#   LAPACKE::LAPACK          - Imported target for the LAPACK library
-#   LAPACKE::CBLAS           - Imported target for the CBLAS library
-#   LAPACKE::BLAS            - Imported target for the BLAS library
+# It sets the following variables:
+#   LAPACK_FOUND               ... true if LAPACKE is found on the system
+#   LAPACK_LIBRARIES           ... full path to LAPACKE library
+#   LAPACK_INCLUDES            ... LAPACKE include directory
 #
 
-# ==============================================================================
-# Copyright 2018 Damien Nguyen <damien.nguyen@alumni.epfl.ch>
-#
-# Distributed under the OSI-approved BSD License (the "License")
-#
-# This software is distributed WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# ==============================================================================
+SET(LAPACKE_ROOT_DIR CACHE STRING
+  "Root directory for custom LAPACK implementation")
 
-set(LAPACKE_SEARCH_PATHS
-  ${LAPACKE_ROOT}
-  $ENV{LAPACKE_ROOT}
-  ${LAPACKE_DIR}
-  $ENV{LAPACKE_DIR}
-  ${CMAKE_PREFIX_PATH}
-  $ENV{CMAKE_PREFIX_PATH}
-  /usr
-  /usr/local/
-  /usr/local/opt # homebrew on mac
-  /opt
-  /opt/local
-  /opt/LAPACKE
-  )
-
-set(LIB_PATH_SUFFIXES
-  lib64
-  lib
-  lib/x86_64-linux-gnu
-  lib32
-  )
-
-set(INC_PATH_SUFFIXES
-  include
-  include/lapack
-  include/lapacke/
-  lapack/include
-  lapacke/include
-  )
-
-if(APPLE)
-  list(APPEND LIB_PATH_SUFFIXES lapack/lib openblas/lib)
-elseif(WIN32)
-  list(APPEND LAPACKE_SEARCH_PATHS "C:/Program Files (x86)/LAPACK")
-  list(APPEND LAPACKE_SEARCH_PATHS "C:/Program Files/LAPACK")
-endif()
-
-# ==============================================================================
-# Prepare some helper variables
-
-set(LAPACKE_INCLUDE_DIRS)
-set(LAPACKE_LIBRARIES)
-set(LAPACKE_REQUIRED_VARS)
-set(LAPACKE_FIND_ALL_COMPONENTS 0)
-
-# ==============================================================================
-
-macro(_find_library_with_header component incname)
-  find_library(LAPACKE_${component}_LIB
-    NAMES ${ARGN}
-    NAMES_PER_DIR
-    PATHS ${LAPACKE_SEARCH_PATHS}
-    PATH_SUFFIXES ${LIB_PATH_SUFFIXES})
-  if(LAPACKE_${component}_LIB)    
-    set(LAPACKE_${component}_LIB_FOUND 1)
-  endif()
-  list(APPEND LAPACKE_REQUIRED_VARS "LAPACKE_${component}_LIB")
-
-  # If necessary, look for the header file as well
-  if(NOT "${incname}" STREQUAL "")
-    find_path(LAPACKE_${component}_INCLUDE_DIR
-      NAMES ${incname}
-      PATHS ${LAPACKE_SEARCH_PATHS}
-      PATH_SUFFIXES ${INC_PATH_SUFFIXES})
-    list(APPEND LAPACKE_REQUIRED_VARS "LAPACKE_${component}_INCLUDE_DIR")
-    if(LAPACKE_${component}_LIB)
-      set(LAPACKE_${component}_INC_FOUND 1)
-    endif()
-  else()
-    set(LAPACKE_${component}_INC_FOUND 1)
-  endif()
-  
-  if(LAPACKE_${component}_LIB_FOUND AND LAPACKE_${component}_INC_FOUND)
-    set(LAPACKE_${component}_FOUND 1)
-  else()
-    set(LAPACKE_${component}_FOUND 0)
-  endif()
-endmacro()
-
-# ------------------------------------------------------------------------------
-
-if(NOT LAPACKE_FIND_COMPONENTS OR LAPACKE_FIND_COMPONENTS STREQUAL "ALL")
-  set(LAPACKE_FIND_ALL_COMPONENTS 1)
-  set(LAPACKE_FIND_COMPONENTS "LAPACKE;LAPACK;CBLAS;BLAS")
-endif(NOT LAPACKE_FIND_COMPONENTS OR LAPACKE_FIND_COMPONENTS STREQUAL "ALL")
-
-# Make sure that all components are in capitals
-set(_tmp_component_list)
-foreach(_comp ${LAPACKE_FIND_COMPONENTS})
-  string(TOUPPER ${_comp} _comp)
-  list(APPEND _tmp_component_list ${_comp})
-endforeach()
-set(LAPACKE_FIND_COMPONENTS ${_tmp_component_list})
-set(_tmp_component_list)
-
-foreach(_comp ${LAPACKE_FIND_COMPONENTS})
-  if(_comp STREQUAL "LAPACKE")
-    _find_library_with_header(${_comp} lapacke.h lapacke liblapacke)
-  elseif(_comp STREQUAL "LAPACKE_H")
-    find_path(LAPACKE_${_comp}_INCLUDE_DIR
-      NAMES lapacke.h
-      PATHS ${LAPACKE_SEARCH_PATHS}
-      PATH_SUFFIXES include lapack/include)
-    list(APPEND LAPACKE_REQUIRED_VARS "LAPACKE_${_comp}_INCLUDE_DIR")
-    if(LAPACKE_${_comp}_LIB)
-      set(LAPACKE_${_comp}_INC_FOUND 1)
-    endif()
-  elseif(_comp STREQUAL "LAPACK")
-    _find_library_with_header(${_comp} "" lapack liblapack)
-  elseif(_comp STREQUAL "CBLAS")
-    _find_library_with_header(${_comp} cblas.h cblas libcblas)
-  elseif(_comp STREQUAL "BLAS")
-    _find_library_with_header(${_comp} "" blas blas)
-  else()
-    message(FATAL_ERROR "Unknown component: ${_comp}")
-  endif()
-  mark_as_advanced(
-    LAPACKE_${_comp}_LIB
-    LAPACKE_${_comp}_INCLUDE_DIR)
-endforeach()
-
-# ==============================================================================
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LAPACKE
-  FOUND_VAR LAPACKE_FOUND
-  REQUIRED_VARS ${LAPACKE_REQUIRED_VARS}
-  HANDLE_COMPONENTS)
-
-# ==============================================================================
-
-if(LAPACKE_FOUND)
-  foreach(_comp ${LAPACKE_FIND_COMPONENTS})
-    list(APPEND LAPACKE_INCLUDE_DIRS ${LAPACKE_${_comp}_INCLUDE_DIR})
-    list(APPEND LAPACKE_LIBRARIES ${LAPACKE_${_comp}_LIB})
-  endforeach()
-  
-  if("${CMAKE_C_COMPILER_ID}" MATCHES ".*Clang.*" OR
-      "${CMAKE_C_COMPILER_ID}" MATCHES ".*GNU.*" OR
-      "${CMAKE_C_COMPILER_ID}" MATCHES ".*Intel.*"
-      ) #NOT MSVC
-    set(MATH_LIB "m")
-    list(APPEND LAPACKE_LIBRARIES m)
-  endif()
-  
-  if(NOT "${LAPACKE_INCLUDE_DIRS}" STREQUAL "")
-    list(REMOVE_DUPLICATES LAPACKE_INCLUDE_DIRS)
+if(NOT LAPACKE_ROOT_DIR)
+  if (ENV{LAPACKEDIR})
+    SET(LAPACKE_ROOT_DIR $ENV{LAPACKEDIR})
   endif()
 
-  # ----------------------------------------------------------------------------
+  if (ENV{LAPACKE_ROOT_DIR})
+    SET(LAPACKE_ROOT_DIR $ENV{LAPACKE_ROOT_DIR})
+  endif()
 
-  # Inspired by FindBoost.cmake
-  foreach(_comp ${LAPACKE_FIND_COMPONENTS})
-    if(NOT TARGET LAPACKE::${_comp} AND LAPACKE_${_comp}_FOUND)
-      get_filename_component(LIB_EXT "${LAPACKE_${_comp}_LIB}" EXT)
-      if(LIB_EXT STREQUAL ".a" OR LIB_EXT STREQUAL ".lib")
-        set(LIB_TYPE STATIC)
-      else()
-        set(LIB_TYPE SHARED)
-      endif()
-      add_library(LAPACKE::${_comp} ${LIB_TYPE} IMPORTED GLOBAL)
-      if(LAPACKE_INCLUDE_DIRS)
-        set_target_properties(LAPACKE::${_comp} PROPERTIES
-          INTERFACE_INCLUDE_DIRECTORIES "${LAPACKE_INCLUDE_DIRS}")
-      endif()
-      if(EXISTS "${LAPACKE_${_comp}_LIB}")
-        set_target_properties(LAPACKE::${_comp} PROPERTIES
-          IMPORTED_LOCATION "${LAPACKE_${_comp}_LIB}")
-      endif()
-      set_target_properties(LAPACKE::${_comp} PROPERTIES
-        INTERFACE_LINK_LIBRARIES "${MATH_LIB}")
-    endif()
-  endforeach()
-
-  # ----------------------------------------------------------------------------
-
-  if(NOT LAPACKE_FIND_QUIETLY)
-    message(STATUS "Found LAPACKE and defined the following imported targets:")
-    foreach(_comp ${LAPACKE_FIND_COMPONENTS})
-      message(STATUS "  - LAPACKE::${_comp}:")
-      message(STATUS "      + include:      ${LAPACKE_INCLUDE_DIRS}")
-      message(STATUS "      + library:      ${LAPACKE_${_comp}_LIB}")
-      message(STATUS "      + dependencies: ${MATH_LIB}")
-    endforeach()
+  if (ENV{MKLROOT})
+    SET(LAPACKE_ROOT_DIR $ENV{MKLROOT})
   endif()
 endif()
 
-# ==============================================================================
+# Check if we can use PkgConfig
+FIND_PACKAGE(PkgConfig)
 
-mark_as_advanced(
-  LAPACKE_FOUND
-  LAPACKE_INCLUDE_DIRS
-  LAPACKE_LIBRARIES
-  )
+#Determine from PKG
+IF(PKG_CONFIG_FOUND AND NOT LAPACKE_ROOT_DIR)
+  PKG_CHECK_MODULES( PC_LAPACKE QUIET "lapacke")
+ENDIF()
+
+IF(PC_LAPACKE_FOUND)
+    FOREACH(PC_LIB ${PC_LAPACKE_LIBRARIES})
+      FIND_LIBRARY(${PC_LIB}_LIBRARY NAMES ${PC_LIB} HINTS ${PC_LAPACKE_LIBRARY_DIRS} )
+      IF (NOT ${PC_LIB}_LIBRARY)
+        MESSAGE(FATAL_ERROR "Something is wrong in your pkg-config file - lib ${PC_LIB} not found in ${PC_LAPACKE_LIBRARY_DIRS}")
+      ENDIF (NOT ${PC_LIB}_LIBRARY)
+      LIST(APPEND LAPACKE_LIB ${${PC_LIB}_LIBRARY})
+    ENDFOREACH(PC_LIB)
+
+    FIND_PATH(
+        LAPACKE_INCLUDES
+        NAMES "lapacke.h"
+        PATHS
+        ${PC_LAPACKE_INCLUDE_DIRS}
+        ${INCLUDE_INSTALL_DIR}
+        /usr/include
+        /usr/local/include
+        /sw/include
+        /opt/local/include
+        DOC "LAPACKE Include Directory"
+        )
+
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(LAPACKE DEFAULT_MSG LAPACKE_LIB)
+    MARK_AS_ADVANCED(LAPACKE_INCLUDES LAPACKE_LIB)
+
+ELSE(PC_LAPACKE_FOUND)
+
+    IF ("${SIZE_OF_VOIDP}" EQUAL 8)
+        SET(MKL_LIB_DIR_SUFFIX "intel64")
+    ELSE()
+        SET(MKL_LIB_DIR_SUFFIX "ia32")
+    ENDIF()
+
+    IF(LAPACKE_ROOT_DIR)
+        #find libs
+        FIND_LIBRARY(
+            LAPACKE_LIB
+            NAMES "lapacke" "LAPACKE" "liblapacke" "mkl_rt"
+            PATHS ${LAPACKE_ROOT_DIR}
+            PATH_SUFFIXES "lib" "lib64" "lib/${MKL_LIB_DIR_SUFFIX}"
+            DOC "LAPACKE Library"
+            NO_DEFAULT_PATH
+            )
+        FIND_LIBRARY(
+            LAPACK_LIB
+            NAMES "lapack" "LAPACK" "liblapack" "mkl_rt"
+            PATHS ${LAPACKE_ROOT_DIR}
+            PATH_SUFFIXES "lib" "lib64" "lib/${MKL_LIB_DIR_SUFFIX}"
+            DOC "LAPACK Library"
+            NO_DEFAULT_PATH
+            )
+        FIND_PATH(
+            LAPACKE_INCLUDES
+            NAMES "lapacke.h" "mkl_lapacke.h"
+            PATHS ${LAPACKE_ROOT_DIR}
+            PATH_SUFFIXES "include"
+            DOC "LAPACKE Include Directory"
+            NO_DEFAULT_PATH
+            )
+    ELSE()
+        FIND_LIBRARY(
+            LAPACKE_LIB
+            NAMES "lapacke" "liblapacke" "openblas" "mkl_rt"
+            PATHS
+            ${PC_LAPACKE_LIBRARY_DIRS}
+            ${LIB_INSTALL_DIR}
+            /opt/intel/mkl/lib/${MKL_LIB_DIR_SUFFIX}
+            /usr/lib64
+            /usr/lib
+            /usr/local/lib64
+            /usr/local/lib
+            /sw/lib
+            /opt/local/lib
+            DOC "LAPACKE Library"
+            )
+        FIND_LIBRARY(
+           LAPACK_LIB
+            NAMES "lapack" "liblapack" "openblas" "mkl_rt"
+            PATHS
+            ${PC_LAPACKE_LIBRARY_DIRS}
+            ${LIB_INSTALL_DIR}
+            /opt/intel/mkl/lib/${MKL_LIB_DIR_SUFFIX}
+            /usr/lib64
+            /usr/lib
+            /usr/local/lib64
+            /usr/local/lib
+            /sw/lib
+            /opt/local/lib
+            DOC "LAPACK Library"
+            )
+        FIND_PATH(
+            LAPACKE_INCLUDES
+            NAMES "lapacke.h" "mkl_lapacke.h"
+            PATHS
+            ${PC_LAPACKE_INCLUDE_DIRS}
+            ${INCLUDE_INSTALL_DIR}
+            /opt/intel/mkl/include
+            /usr/include
+            /usr/local/include
+            /sw/include
+            /opt/local/include
+            DOC "LAPACKE Include Directory"
+            PATH_SUFFIXES
+            lapacke
+            )
+    ENDIF(LAPACKE_ROOT_DIR)
+ENDIF(PC_LAPACKE_FOUND)
+
+IF(PC_LAPACKE_FOUND OR (LAPACKE_LIB AND LAPACK_LIB))
+    SET(LAPACK_LIBRARIES ${LAPACKE_LIB} ${LAPACK_LIB})
+ENDIF()
+IF(LAPACKE_INCLUDES)
+    SET(LAPACK_INCLUDE_DIR ${LAPACKE_INCLUDES})
+ENDIF()
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LAPACKE DEFAULT_MSG
+                                  LAPACK_INCLUDE_DIR LAPACK_LIBRARIES)
+
+MARK_AS_ADVANCED(
+  LAPACKE_ROOT_DIR
+  LAPACK_INCLUDES
+  LAPACK_LIBRARIES
+  LAPACK_LIB
+  LAPACKE_INCLUDES
+  LAPACKE_LIB
+  lapack_LIBRARY
+  lapacke_LIBRARY)
+
+if(LAPACK_FOUND)
+  add_library(LAPACKE::LAPACKE UNKNOWN IMPORTED)
+  set_target_properties(LAPACKE::LAPACKE PROPERTIES
+      IMPORTED_LINK_INTERFACE_LANGUAGE "C"
+      IMPORTED_LOCATION "${LAPACK_LIBRARIES}"
+      INTERFACE_INCLUDE_DIRECTORIES "${LAPACK_INCLUDE_DIR}"
+    )
+endif(LAPACK_FOUND)
