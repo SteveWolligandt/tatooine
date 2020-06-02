e8bcebb300849f1ddaf94471e361f85460f45156
Steve Wolligandt <stevewolligandt@gmail.com>
2020-06-02 13:55:03 +0200

    added inverse function for matrices

--- include/tatooine/tensor.h
+++ include/tatooine/tensor.h
@@ -741,7 +741,197 @@ using mat4 = mat<double, 4, 4>;
 //==============================================================================
 // operations
 //==============================================================================
+/// invert symmetric matrix
+/// A = [a,b]
+///     [b,c]
+template <typename Tensor, typename Real>
+constexpr auto inv_sym(const base_tensor<Tensor, Real, 2, 2>& A) {
+  const auto& a = A(0, 0);
+  const auto& b = A(1, 0);
+  const auto& c = A(1, 1);
+  const auto  d = 1 / (a * c - b * b);
+  const auto  e = -b * d;
+  return mat{{c * d, e    },
+             {e,     a * d}};
+}
+//------------------------------------------------------------------------------
+/// invert matrix
+/// A = [a,b]
+///     [c,d]
+template <typename Tensor, typename Real>
+constexpr auto inv(const base_tensor<Tensor, Real, 2, 2>& A) {
+  const auto& b = A(0, 1);
+  const auto& c = A(1, 0);
+  if (std::abs(b - c) < 1e-10) { return inv_sym(A); }
+  const auto& a = A(0, 0);
+  const auto& d = A(1, 1);
+  const auto  e = 1/(a * d - b * c);
+  return mat{{ d * e, -b * e},
+             {-c * e,  a * e}};
+}
+//------------------------------------------------------------------------------
+/// invert symmetric matrix
+/// A = [a,b,c]
+///     [b,d,e]
+///     [c,e,f]
+template <typename Tensor, typename Real>
+constexpr auto inv_sym(const base_tensor<Tensor, Real, 3, 3>& A) {
+  const auto& a = A(0, 0);
+  const auto& b = A(1, 0);
+  const auto& c = A(2, 0);
+  const auto& d = A(1, 1);
+  const auto& e = A(2, 1);
+  const auto& f = A(2, 2);
+  const auto  div =
+      1 / ((a * d - b * b) * f - a * e * e + 2 * b * c * e - c * c * d);
+  return mat{
+      { (d * f - e * e) * div, -(b * f - c * e) * div,  (b * e - c * d) * div},
+      {-(b * f - c * e) * div,  (a * f - c * c) * div, -(a * e - b * c) * div},
+      { (b * e - c * d) * div, -(a * e - b * c) * div,  (a * d - b * b) * div}};
+}
+//------------------------------------------------------------------------------
+/// invert matrix
+/// A = [a,b,c]
+///     [d,e,f]
+///     [g,h,i]
+template <typename Tensor, typename Real>
+constexpr auto inv(const base_tensor<Tensor, Real, 3, 3>& A) {
+  const auto& b = A(0, 1);
+  const auto& c = A(0, 2);
+  const auto& d = A(1, 0);
+  const auto& g = A(2, 0);
+  const auto& f = A(1, 2);
+  const auto& h = A(2, 1);
+  if (std::abs(b - d) < 1e-10 && std::abs(c - g) < 1e-10 &&
+      std::abs(f - h) < 1e-10) {
+    return inv_sym(A);
+  }
+  const auto& a = A(0, 0);
+  const auto& e = A(1, 1);
+  const auto& i = A(2, 2);
+  const auto  div =
+      1 / ((a * e - b * d) * i + (c * d - a * f) * h + (b * f - c * e) * g);
+  return mat{
+      { (e * i - f * h) * div, -(b * i - c * h) * div,  (b * f - c * e) * div},
+      {-(d * i - f * g) * div,  (a * i - c * g) * div, -(a * f - c * d) * div},
+      { (d * h - e * g) * div, -(a * h - b * g) * div,  (a * e - b * d) * div}};
+}
+//------------------------------------------------------------------------------
+/// invert symmetric matrix
+/// A = [a,b,c,d]
+///     [b,e,f,g]
+///     [c,f,h,i]
+///     [d,g,i,j]
+template <typename Tensor, typename Real>
+constexpr auto inv_sym(const base_tensor<Tensor, Real, 4, 4>& A) {
+  const auto& a = A(0, 0);
+  const auto& b = A(1, 0);
+  const auto& c = A(2, 0);
+  const auto& d = A(3, 0);
+  const auto& e = A(1, 1);
+  const auto& f = A(2, 1);
+  const auto& g = A(3, 1);
+  const auto& h = A(2, 2);
+  const auto& i = A(3, 2);
+  const auto& j = A(3, 3);
+  const auto  div =
+      1 / (((a * e - b * b) * h - a * f * f + 2 * b * c * f - c * c * e) * j +
+           (b * b - a * e) * i * i +
+           ((2 * a * f - 2 * b * c) * g - 2 * b * d * f + 2 * c * d * e) * i +
+           (-a * g * g + 2 * b * d * g - d * d * e) * h + c * c * g * g -
+           2 * c * d * f * g + d * d * f * f);
+  return mat{
+      {((e * h - f * f) * j - e * i * i + 2 * f * g * i - g * g * h) * div,
+       -((b * h - c * f) * j - b * i * i + (c * g + d * f) * i - d * g * h) *
+           div,
+       ((b * f - c * e) * j + (d * e - b * g) * i + c * g * g - d * f * g) *
+           div,
+       -((b * f - c * e) * i + (d * e - b * g) * h + c * f * g - d * f * f) *
+           div},
+      {-((b * h - c * f) * j - b * i * i + (c * g + d * f) * i - d * g * h) *
+           div,
+       ((a * h - c * c) * j - a * i * i + 2 * c * d * i - d * d * h) * div,
+       -((a * f - b * c) * j + (b * d - a * g) * i + c * d * g - d * d * f) *
+           div,
+       ((a * f - b * c) * i + (b * d - a * g) * h + c * c * g - c * d * f) *
+           div},
+      {((b * f - c * e) * j + (d * e - b * g) * i + c * g * g - d * f * g) *
+           div,
+       -((a * f - b * c) * j + (b * d - a * g) * i + c * d * g - d * d * f) *
+           div,
+       ((a * e - b * b) * j - a * g * g + 2 * b * d * g - d * d * e) * div,
+       -((a * e - b * b) * i + (b * c - a * f) * g + b * d * f - c * d * e) *
+           div},
+      {-((b * f - c * e) * i + (d * e - b * g) * h + c * f * g - d * f * f) *
+           div,
+       ((a * f - b * c) * i + (b * d - a * g) * h + c * c * g - c * d * f) *
+           div,
+       -((a * e - b * b) * i + (b * c - a * f) * g + b * d * f - c * d * e) *
+           div,
+       ((a * e - b * b) * h - a * f * f + 2 * b * c * f - c * c * e) * div}};
+}
+//------------------------------------------------------------------------------
+/// invert matrix
+/// A = [a,b,c,d]
+///     [e,f,g,h]
+///     [i,j,k,l]
+///     [m,n,o,p]
+template <typename Tensor, typename Real>
+constexpr auto inv(const base_tensor<Tensor, Real, 4, 4>& A) {
+  const auto& b = A(0, 1);
+  const auto& c = A(0, 2);
+  const auto& d = A(0, 3);
+  const auto& e = A(1, 0);
+  const auto& g = A(1, 2);
+  const auto& h = A(1, 3);
+  const auto& i = A(2, 0);
+  const auto& j = A(2, 1);
+  const auto& l = A(2, 3);
+  const auto& m = A(3, 0);
+  const auto& n = A(3, 1);
+  const auto& o = A(3, 2);
 
+  if (std::abs(b - e) < 1e-10 &&
+      std::abs(c - i) < 1e-10 &&
+      std::abs(d - m) < 1e-10 &&
+      std::abs(g - j) < 1e-10 &&
+      std::abs(h - n) < 1e-10 &&
+      std::abs(l - o) < 1e-10) {
+    return inv_sym(A);
+  }
+
+  const auto& a = A(0, 0);
+  const auto& f = A(1, 1);
+  const auto& k = A(2, 2);
+  const auto& p = A(3, 3);
+  const auto  div =
+      1 /
+      ((((a * f - b * e) * k + (c * e - a * g) * j + (b * g - c * f) * i) * p +
+        ((b * e - a * f) * l + (a * h - d * e) * j + (d * f - b * h) * i) * o +
+        ((a * g - c * e) * l + (d * e - a * h) * k + (c * h - d * g) * i) * n +
+        ((c * f - b * g) * l + (b * h - d * f) * k + (d * g - c * h) * j) * m));
+  return mat{
+      {((f * k - g * j) * p + (h * j - f * l) * o + (g * l - h * k) * n) * div,
+       -((b * k - c * j) * p + (d * j - b * l) * o + (c * l - d * k) * n) * div,
+       ((b * g - c * f) * p + (d * f - b * h) * o + (c * h - d * g) * n) * div,
+       -((b * g - c * f) * l + (d * f - b * h) * k + (c * h - d * g) * j) *
+           div},
+      {-((e * k - g * i) * p + (h * i - e * l) * o + (g * l - h * k) * m) * div,
+       ((a * k - c * i) * p + (d * i - a * l) * o + (c * l - d * k) * m) * div,
+       -((a * g - c * e) * p + (d * e - a * h) * o + (c * h - d * g) * m) * div,
+       ((a * g - c * e) * l + (d * e - a * h) * k + (c * h - d * g) * i) * div},
+      {((e * j - f * i) * p + (h * i - e * l) * n + (f * l - h * j) * m) * div,
+       -((a * j - b * i) * p + (d * i - a * l) * n + (b * l - d * j) * m) * div,
+       ((a * f - b * e) * p + (d * e - a * h) * n + (b * h - d * f) * m) * div,
+       -((a * f - b * e) * l + (d * e - a * h) * j + (b * h - d * f) * i) *
+           div},
+      {-((e * j - f * i) * o + (g * i - e * k) * n + (f * k - g * j) * m) * div,
+       ((a * j - b * i) * o + (c * i - a * k) * n + (b * k - c * j) * m) * div,
+       -((a * f - b * e) * o + (c * e - a * g) * n + (b * g - c * f) * m) * div,
+       ((a * f - b * e) * k + (c * e - a * g) * j + (b * g - c * f) * i) *
+           div}};
+}
+//==============================================================================
 /// Returns the cosine of the angle of two normalized vectors.
 template <typename Tensor0, typename Tensor1, typename Real0, typename Real1,
           size_t N>
