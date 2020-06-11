20a3e64585a29cda3f8445a150b49d6c2ca512d2
Steve Wolligandt <stevewolligandt@gmail.com>
2020-06-11 17:00:53 +0200

    netcdf wrapper working for simplest write/read example

--- include/tatooine/netcdf.h
+++ include/tatooine/netcdf.h
@@ -2,11 +2,78 @@
 #define TATOOINE_NETCDF_H
 //==============================================================================
 #include <netcdf>
+#include <memory>
 #include <vector>
 //==============================================================================
 namespace tatooine::netcdf {
 //==============================================================================
-
+template <typename T>
+auto to_nc_type();
+template <>
+auto to_nc_type<int>() {return netCDF::ncInt;}
+//==============================================================================
+class group {};
+//==============================================================================
+class attribute {};
+//==============================================================================
+template <typename T>
+class variable {
+  mutable std::shared_ptr<netCDF::NcFile> m_file;
+  netCDF::NcVar                   m_variable;
+  //============================================================================
+ public:
+  variable(std::shared_ptr<netCDF::NcFile>& file, netCDF::NcVar&& var)
+      : m_file{file}, m_variable{std::move(var)} {}
+  //============================================================================
+  auto put(T const* const data) { m_variable.putVar(data); }
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  auto put(std::vector<T> const& data){
+    m_variable.putVar(data.data());
+  }
+  //----------------------------------------------------------------------------
+  auto to_vector() const {
+    size_t size = 1;
+    for (auto const& dim : dimensions()) { size *= dim.getSize(); }
+    std::vector<T> data(size);
+    m_variable.getVar(data.data());
+    return data;
+  }
+  //----------------------------------------------------------------------------
+  auto is_null() const { return m_variable.isNull(); }
+  //----------------------------------------------------------------------------
+  auto num_dimensions() const { return m_variable.getDimCount(); }
+  //----------------------------------------------------------------------------
+  auto dimension(int i) const { return m_variable.getDim(i); }
+  //----------------------------------------------------------------------------
+  auto dimensions() const { return m_variable.getDims(); }
+};
+//==============================================================================
+class dimension {};
+//==============================================================================
+class file {
+  mutable std::shared_ptr<netCDF::NcFile> m_file;
+  //============================================================================
+ public:
+  template <typename... Ts>
+  file(std::string const& path, Ts&&... ts)
+      : m_file{new netCDF::NcFile{path, std::forward<Ts>(ts)...}} {}
+  //============================================================================
+  template <typename T>
+  auto add_variable(std::string const&                variable_name,
+                    std::vector<netCDF::NcDim> const& dims) {
+    return variable<T>{
+        m_file, m_file->addVar(variable_name, to_nc_type<T>(), dims)};
+  }
+  //----------------------------------------------------------------------------
+  template <typename T>
+  auto get_variable(std::string const& variable_name) const {
+    return variable<T>{m_file, m_file->getVar(variable_name)};
+  }
+  //----------------------------------------------------------------------------
+  auto add_dimension(std::string const& dimension_name, size_t const size) {
+    return m_file->addDim(dimension_name, size);
+  }
+};
 //==============================================================================
 }  // namespace tatooine
 //==============================================================================
--- test/netcdf.cpp
+++ test/netcdf.cpp
@@ -1,60 +1,48 @@
 #include <tatooine/netcdf.h>
+
 #include <catch2/catch.hpp>
 //==============================================================================
-namespace tatooine {
+namespace tatooine::netcdf {
 //==============================================================================
 using namespace netCDF::exceptions;
 TEST_CASE("netcdf_write_read", "[netcdf][read][write]") {
-  std::string const file_path = "simple_xy.nc";
-  std::string const xdim_name = "x";
-  std::string const ydim_name = "y";
-  std::string const data_name = "data";
+  std::string const file_path     = "simple_xy.nc";
+  std::string const xdim_name     = "x";
+  std::string const ydim_name     = "y";
+  std::string const variable_name = "data";
   // We are reading 2D data, a 6 x 12 grid.
   size_t constexpr NX = 6;
   size_t constexpr NY = 12;
 
-  {  // write block
-    
-    // create some data
-    int dataOut[NX][NY];
-    for (size_t i = 0; i < NX; ++i) {
-      for (size_t j = 0; j < NY; ++j) { dataOut[i][j] = i * NY + j; }
+  std::vector<int> data_out(NX * NY);
+  // create some data
+  for (size_t i = 0; i < NX; ++i) {
+    for (size_t j = 0; j < NY; ++j) {
+      size_t idx    = i * NY + j;
+      data_out[idx] = idx;
     }
-
-    try {
-      // Create the file. The Replace parameter tells netCDF to overwrite
-      // this file, if it already exists.
-      netCDF::NcFile data_file{file_path, netCDF::NcFile::replace};
-
-      data_file
-          .addVar(
-              data_name, netCDF::ncInt,
-              {data_file.addDim(xdim_name, NX), data_file.addDim(ydim_name, NY)})
-          .putVar(dataOut);
-
-    } catch (NcException& e) {}
   }
+  file f_out{file_path, netCDF::NcFile::replace};
 
-  {  // read block
-    int dataIn[NX][NY];
-    try {
-      // Open the file for read access
-      netCDF::NcFile data_file{file_path, netCDF::NcFile::read};
+  f_out
+      .add_variable<int>(variable_name, {f_out.add_dimension(xdim_name, NX),
+                                         f_out.add_dimension(ydim_name, NY)})
+      .put(data_out);
 
-      // Retrieve the variable
-      auto data = data_file.getVar(data_name);
-      REQUIRE(!data.isNull());
-      data.getVar(dataIn);
+  file f_in{file_path, netCDF::NcFile::read};
+  // Retrieve the variable
+  auto var = f_in.get_variable<int>(variable_name);
+  REQUIRE(!var.is_null());
+  auto data_in = var.to_vector();
 
-      // Check the values.
-      for (size_t i = 0; i < NX; ++i) {
-        for (size_t j = 0; j < NY; ++j) {
-          REQUIRE(dataIn[i][j] == i * NY + j);
-        }
-      }
-    } catch (NcException& e) {}
+  // Check the values.
+  for (size_t i = 0; i < NX; ++i) {
+    for (size_t j = 0; j < NY; ++j) {
+      size_t idx = i * NY + j;
+      REQUIRE(data_in[idx] == data_out[idx]);
+    }
   }
 }
 //==============================================================================
-}  // namespace tatooine
+}  // namespace tatooine::netcdf
 //==============================================================================
