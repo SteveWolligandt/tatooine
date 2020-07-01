48fdb07bac6294d467863394191c45ed1e239324
Steve Wolligandt <steve@isg.cs.ovgu.de>
2020-07-01 18:02:52 +0200

    taking condition number=2

--- include/tatooine/autonomous_particle.h
+++ include/tatooine/autonomous_particle.h
@@ -2,8 +2,8 @@
 #define TATOOINE_AUTONOMOUS_PARTICLES
 //==============================================================================
 #include <tatooine/field.h>
-#include <tatooine/vtk_legacy.h>
 #include <tatooine/tensor.h>
+#include <tatooine/vtk_legacy.h>
 
 #include "concepts.h"
 //==============================================================================
@@ -23,18 +23,17 @@ struct autonomous_particle {
   using mat_t  = mat<real_t, num_dimensions(), num_dimensions()>;
   using pos_t  = vec_t;
 
-  static constexpr real_t max_cond = 9.01;
+  static constexpr real_t max_cond = 4.01;
 
   //----------------------------------------------------------------------------
   // members
   //----------------------------------------------------------------------------
  private:
-  flowmap_t m_flowmap;
+  flowmap_t m_phi;
   pos_t     m_x0, m_x1, m_prev_pos;
   real_t    m_t1, m_prev_time;
   mat_t     m_nabla_phi1;
-  size_t    m_level;
-  real_t    m_start_radius;
+  real_t    m_current_radius;
 
   //----------------------------------------------------------------------------
   // ctors
@@ -44,37 +43,34 @@ struct autonomous_particle {
   autonomous_particle(const vectorfield<V, VReal, 2>&      v,
                       vec<RealX0, num_dimensions()> const& x0,
                       arithmetic auto const                start_time,
-                      arithmetic auto const                start_radius)
-      : autonomous_particle{flowmap(v), x0, start_time, start_radius} {}
+                      arithmetic auto const                current_radius)
+      : autonomous_particle{flowmap(v), x0, start_time, current_radius} {}
   //----------------------------------------------------------------------------
   template <arithmetic RealX0>
-  autonomous_particle(flowmap_t                            flowmap,
-                      vec<RealX0, num_dimensions()> const& x0,
-                      arithmetic auto const                start_time,
-                      arithmetic auto const                start_radius)
-      : m_flowmap{std::move(flowmap)},
+  autonomous_particle(flowmap_t phi, vec<RealX0, num_dimensions()> const& x0,
+                      arithmetic auto const start_time,
+                      arithmetic auto const current_radius)
+      : m_phi{std::move(phi)},
         m_x0{x0},
         m_x1{x0},
         m_prev_pos{x0},
         m_t1{static_cast<real_t>(start_time)},
         m_prev_time{static_cast<real_t>(start_time)},
         m_nabla_phi1{mat_t::eye()},
-        m_level{0},
-        m_start_radius{start_radius} {}
+        m_current_radius{current_radius} {}
   //----------------------------------------------------------------------------
-  autonomous_particle(flowmap_t flowmap, pos_t const& x0, pos_t const& x1,
+  autonomous_particle(flowmap_t phi, pos_t const& x0, pos_t const& x1,
                       pos_t const& prev_pos, real_t const t1,
                       real_t const prev_time, mat_t const& nabla_phi1,
-                      size_t const level, real_t const start_radius)
-      : m_flowmap{std::move(flowmap)},
+                      real_t const current_radius)
+      : m_phi{std::move(phi)},
         m_x0{x0},
         m_x1{x1},
         m_prev_pos{prev_pos},
         m_t1{t1},
         m_prev_time{prev_time},
         m_nabla_phi1{nabla_phi1},
-        m_level{level},
-        m_start_radius{start_radius} {}
+        m_current_radius{current_radius} {}
   //----------------------------------------------------------------------------
  public:
   autonomous_particle(const autonomous_particle&)     = default;
@@ -96,37 +92,31 @@ struct autonomous_particle {
   auto t1() const { return m_t1; }
   auto previous_time() const { return m_prev_time; }
   auto nabla_phi1() const -> auto const& { return m_nabla_phi1; }
-  auto level() const { return m_level; }
-  auto current_radius() const {
-    static real_t const sqrt3 = std::sqrt(real_t(3));
-    return m_start_radius * std::pow<real_t>(1 / sqrt3, m_level);
-  }
-  auto start_radius() const { return m_start_radius; }
-  auto get_flowmap() const -> auto const& { return m_flowmap; }
-  auto get_flowmap() -> auto& { return m_flowmap; }
+  auto current_radius() const { return m_current_radius; }
+  auto phi() const -> auto const& { return m_phi; }
+  auto phi() -> auto& { return m_phi; }
 
   //----------------------------------------------------------------------------
   // methods
   //----------------------------------------------------------------------------
  public:
   auto integrate(real_t tau_step, real_t const max_t) const {
-    std::vector<this_t> particles{*this};
+    std::vector<this_t>                             particles{*this};
     std::vector<line<real_t, num_dimensions() + 1>> ellipses;
 
-    size_t n         = 0;
-    size_t start_idx = 0;
+    size_t size_before = size(particles);
+    size_t start_idx   = 0;
     do {
-      n                   = size(particles);
-      auto const cur_size = size(particles);
       // this is necessary because after a resize of particles the currently
       // used adresses are not valid anymore
-      particles.reserve(size(particles) + (cur_size - start_idx) * 3);
-      for (size_t i = start_idx; i < cur_size; ++i) {
+      particles.reserve(size(particles) + size(particles) * 3);
+      size_before = size(particles);
+      for (size_t i = start_idx; i < size_before; ++i) {
         particles[i].integrate_until_split(tau_step, particles, ellipses,
                                            max_t);
       }
-      start_idx = cur_size;
-    } while (n != size(particles));
+      start_idx = size_before;
+    } while (size_before != size(particles));
     return std::pair{std::move(particles), std::move(ellipses)};
   }
   //----------------------------------------------------------------------------
@@ -136,63 +126,69 @@ struct autonomous_particle {
       real_t const                                     max_t) const {
     // add initial circle
     auto&        ellipse = ellipses.emplace_back();
-    size_t const n       = 20;
-    auto const   r       = current_radius();
+    size_t const n       = 100;
     for (auto t : linspace{0.0, M_PI * 2, n}) {
-      ellipse.push_back(
-          vec{std::cos(t) * r + m_x1(0), std::sin(t) * r + m_x1(1), t1()});
+      ellipse.push_back(vec{std::cos(t) * m_current_radius + m_x1(0),
+                            std::sin(t) * m_current_radius + m_x1(1), t1()});
     }
 
     if (m_t1 >= max_t) { return; }
-    static real_t const twothirds = real_t(2) / real_t(3);
-    static real_t const sqrt3     = std::sqrt(real_t(3));
+    static real_t const threequarters = real_t(3) / real_t(4);
+    // static real_t const sqrt3     = std::sqrt(real_t(3));
 
     real_t tau       = 0;
-    auto   nabla_phi = diff(m_flowmap);
+    auto   nabla_phi = diff(m_phi);
     if constexpr (is_flowmap_gradient_central_differences(nabla_phi)) {
       nabla_phi.set_epsilon(current_radius());
     }
-    real_t cond = 1;
-    std::pair<mat<real_t, num_dimensions(), num_dimensions()>,
-              vec<real_t, num_dimensions()>>
-                eig;
-    auto const& eigvecs = eig.first;
-    auto const& eigvals = eig.second;
-
+    real_t                                   cond = 1;
     typename decltype(nabla_phi)::gradient_t nabla_phi2;
-    while (cond < 9 && m_t1 + tau < max_t) {
+    using eig_t =
+        decltype(eigenvectors_sym(nabla_phi2 * transpose(nabla_phi2)));
+    eig_t                        eig_left;
+    [[maybe_unused]] auto const& eigvecs_left_cauchy_green = eig_left.first;
+    [[maybe_unused]] auto const& eigvals_left_cauchy_green = eig_left.second;
+    // eig_t                        eig_right;
+    //[[maybe_unused]] auto const& eigvecs_right_cauchy_green = eig_right.first;
+    //[[maybe_unused]] auto const& eigvals_right_cauchy_green =
+    // eig_right.second;
+
+    while (cond < 4 && m_t1 + tau < max_t) {
       tau += tau_step;
       if (m_t1 + tau > max_t) { tau = max_t - m_t1; }
       nabla_phi2 = nabla_phi(m_x1, m_t1, tau);
-      eig        = eigenvectors_sym(nabla_phi2 * transpose(nabla_phi2));
+      eig_left   = eigenvectors_sym(nabla_phi2 * transpose(nabla_phi2));
+      // eig_right  = eigenvectors_sym(transpose(nabla_phi2) * nabla_phi2);
 
-      cond = eigvals(1) / eigvals(0);
-      // if (cond > max_cond) {
-      //  cond = 0;
-      //  tau -= tau_step;
-      //  tau_step *= 0.5;
-      //}
+      cond = eigvals_left_cauchy_green(1) / eigvals_left_cauchy_green(0);
+      if (cond > max_cond) {
+        cond = 0;
+        tau -= tau_step;
+        tau_step *= 0.5;
+      }
     }
     mat_t const  fmg2fmg1 = nabla_phi2 * m_nabla_phi1;
-    pos_t const  x2       = m_flowmap(m_x1, m_t1, tau);
+    pos_t const  x2       = m_phi(m_x1, m_t1, tau);
     real_t const t2       = m_t1 + tau;
-    if (cond > 9) {
-      vec_t const offset2 =
-          twothirds * sqrt3 * current_radius() * normalize(eigvecs.col(1));
+    if (cond > 4) {
+      vec_t const offset2 = threequarters * std::sqrt(2.0) * m_current_radius *
+                            normalize(eigvecs_left_cauchy_green.col(1));
       vec_t const offset0 = inv(fmg2fmg1) * offset2;
 
-      //particles.emplace_back(m_flowmap, m_x0 - offset0, x2 - offset2, m_x1, t2, m_t1,
-      //                       fmg2fmg1, m_level + 1, m_start_radius);
-      //particles.emplace_back(m_flowmap, m_x0, x2, m_x1, t2, m_t1, fmg2fmg1, m_level + 1,
-      //                       m_start_radius);
-      particles.emplace_back(m_flowmap, m_x0 + offset0, x2 + offset2, m_x1, t2, m_t1,
-                             fmg2fmg1, m_level + 1, m_start_radius);
+      particles.emplace_back(m_phi, m_x0 - offset0, x2 - offset2, m_x1, t2,
+                             m_t1, fmg2fmg1,
+                             m_current_radius * std::sqrt(2) / 4);
+      particles.emplace_back(m_phi, m_x0, x2, m_x1, t2, m_t1, fmg2fmg1,
+                             m_current_radius * std::sqrt(2) / 2);
+      particles.emplace_back(m_phi, m_x0 + offset0, x2 + offset2, m_x1, t2,
+                             m_t1, fmg2fmg1,
+                             m_current_radius * std::sqrt(2) / 4);
 
-      auto&        ellipse = ellipses.emplace_back();
-      size_t const n       = 20;
-      auto const   a       = eigvals(1) * r;
-      auto const   b       = eigvals(0) * r;
-      auto const alpha = angle(vec{1.0, 0.0}, eigvecs.col(1));
+      auto& ellipse = ellipses.emplace_back();
+
+      auto const a = std::sqrt(eigvals_left_cauchy_green(1)) * m_current_radius;
+      auto const b = std::sqrt(eigvals_left_cauchy_green(0)) * m_current_radius;
+      auto const alpha = angle(vec{1.0, 0.0}, eigvecs_left_cauchy_green.col(1));
       for (auto t : linspace{0.0, M_PI * 2, n}) {
         vec p{std::cos(t) * a, std::sin(t) * b};
 
@@ -204,8 +200,8 @@ struct autonomous_particle {
         ellipse.push_back(vec{p(0), p(1), t2});
       }
     } else {
-      //particles.emplace_back(m_flowmap, m_x0, x2, m_x1, t2, m_t1, fmg2fmg1, m_level + 1,
-      //                       m_start_radius);
+      // particles.emplace_back(m_phi, m_x0, x2, m_x1, t2, m_t1, fmg2fmg1,
+      //                       m_current_radius);
     }
   }
 };
@@ -213,9 +209,8 @@ struct autonomous_particle {
 // deduction guides
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 template <typename V, typename VReal, arithmetic RealX0, size_t N>
-autonomous_particle(const vectorfield<V, VReal, 2>& v,
-                    vec<RealX0, N> const&           , arithmetic auto const,
-                    arithmetic auto const)
+autonomous_particle(const vectorfield<V, VReal, 2>& v, vec<RealX0, N> const&,
+                    arithmetic auto const, arithmetic auto const)
     -> autonomous_particle<decltype(flowmap(v))>;
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 template <fixed_dims_flowmap_c<2> Flowmap, arithmetic RealX0, size_t N>
--- test/autonomous_particle.cpp
+++ test/autonomous_particle.cpp
@@ -24,7 +24,7 @@ void autonomous_particle_write_vtk(std::string const&              name,
       auto const x0 = g(x, y);
 
       autonomous_particle p0{v, x0, t0, radius};
-      auto const          [particles, ellipses] = p0.integrate(tau_step, t1);
+      auto const [particles, ellipses] = p0.integrate(tau_step, t1);
 
       write_vtk(particles, t0,
                 name + "_autonomous_particle_paths_forward" +
@@ -38,17 +38,18 @@ void autonomous_particle_write_vtk(std::string const&              name,
 template <typename V, std::floating_point VReal, std::floating_point X0Real>
 void autonomous_particles_test_backward_integation_distance(
     std::string const& name, vectorfield<V, VReal, 2> const& v,
-    vec<X0Real, 2> const& x0, std::floating_point auto const radius,
+    vec<X0Real, 2> const& x0, std::floating_point auto const start_radius,
     arithmetic auto t0, arithmetic auto t1, arithmetic auto tau_step) {
-  [[maybe_unused]] double const eps = 1e-10;
+  [[maybe_unused]] double const eps = 1e-5;
   std::vector<parameterized_line<VReal, 3, interpolation::linear>>
                             integral_curves;
-  autonomous_particle const p0{v, x0, t0, radius};
+  autonomous_particle const p0{v, x0, t0, start_radius};
   auto [particles, ellipses] = p0.integrate(tau_step, t1);
+  std::vector<line<double, 3>> formula_back;
   for (auto const& particle : particles) {
     auto const     total_integration_length = t0 - particle.t1();
     auto&          integral_curve           = integral_curves.emplace_back();
-    decltype(auto) phi                      = particle.get_flowmap();
+    decltype(auto) phi                      = particle.phi();
     auto           stepped_back_integration0 =
         phi(particle.x1(), particle.t1(), total_integration_length);
     for (auto const tau : linspace<VReal>{
@@ -59,34 +60,38 @@ void autonomous_particles_test_backward_integation_distance(
                                particle.t1() + tau);
     }
 
-    //auto const back_integration = particle.get_flowmap()(
-    //    particle.x1(), particle.t1(), total_integration_length);
+    auto& back_calc = formula_back.emplace_back();
+    back_calc.push_back(vec{particle.x0(0), particle.x0(1), t0});
+    back_calc.push_back(vec{particle.x1(0), particle.x1(1), particle.t1()});
 
     {
       INFO("Runge Kutta back step in initial circle")
       CAPTURE(particle.x1(), particle.t1(), total_integration_length,
-              particle.level(), particle.x0(), particle.start_radius(),
+              particle.x0(), particle.current_radius(),
               stepped_back_integration0);
       CHECK(tatooine::distance(stepped_back_integration0, p0.x0()) <=
-            particle.start_radius());
+            start_radius);
     }
 
-    //{
-    //  INFO("Formula back step in initial circle")
-    //  CAPTURE(particle.x1(), particle.t1(), total_integration_length,
-    //          particle.level(), particle.x0(), particle.start_radius());
-    //  CHECK(tatooine::distance(back_integration, p0.x0()) < p0.start_radius());
-    //}
+    {
+      INFO("Formula back step in initial circle")
+      CAPTURE(particle.x1(), particle.t1(), total_integration_length,
+              particle.x0(), particle.current_radius());
+      CHECK(tatooine::distance(stepped_back_integration0, p0.x0()) <
+            start_radius);
+    }
 
-    //{
-    //  INFO("Runge Kutta back step and formula back step approximately equal")
-    //  CAPTURE(particle.x1(), particle.t1(), total_integration_length,
-    //          particle.level(), particle.x0(),particle.start_radius(), back_integration);
-    //  CHECK(tatooine::distance(back_integration, particle.x0()) < eps);
-    //}
+    {
+      INFO("Runge Kutta back step and formula back step approximately equal")
+      CAPTURE(particle.x1(), particle.t1(), total_integration_length,
+              particle.x0(), particle.current_radius(),
+              stepped_back_integration0);
+      CHECK(tatooine::distance(stepped_back_integration0, particle.x0()) < eps);
+    }
   }
 
   write_vtk(integral_curves, name + "_back_integration.vtk");
+  write_vtk(formula_back, name + "_formula_back.vtk");
   write_vtk(ellipses, name + "_ellipses.vtk");
 }
 //------------------------------------------------------------------------------
@@ -94,13 +99,13 @@ template <typename V, std::floating_point VReal, std::floating_point X0Real>
 void autonomous_particles_test_backward_integation_distance(
     std::string const& name, vectorfield<V, VReal, 2> const& v,
     std::vector<vec<X0Real, 2>> const& x0s,
-    std::floating_point auto const radius, arithmetic auto t0,
+    std::floating_point auto const start_radius, arithmetic auto t0,
     arithmetic auto t1, arithmetic auto tau_step) {
   [[maybe_unused]] double const eps = 1e-10;
   std::vector<parameterized_line<VReal, 3, interpolation::linear>>
       integral_curves;
   for (auto const& x0 : x0s) {
-    autonomous_particle const p0{v, x0, t0, radius};
+    autonomous_particle const p0{v, x0, t0, start_radius};
     auto [particles, ellipses] = p0.integrate(tau_step, t1);
     for (auto const& particle : particles) {
       auto const total_integration_length = t0 - particle.t1();
@@ -108,19 +113,18 @@ void autonomous_particles_test_backward_integation_distance(
         auto& integral_curve = integral_curves.emplace_back();
         for (auto const tau :
              linspace<VReal>{total_integration_length, VReal{0}, 10}) {
-          auto const x =
-              particle.get_flowmap()(particle.x1(), particle.t1(), tau);
+          auto const x = particle.phi()(particle.x1(), particle.t1(), tau);
           integral_curve.push_back(vec{x(0), x(1), particle.t1() + tau},
                                    particle.t1() + tau);
         }
       }
 
-      auto const back_integration = particle.get_flowmap()(
-          particle.x1(), particle.t1(), total_integration_length);
+      auto const back_integration = particle.phi()(particle.x1(), particle.t1(),
+                                                   total_integration_length);
 
       auto const distance = tatooine::distance(back_integration, particle.x0());
       CAPTURE(particle.x1(), particle.t1(), total_integration_length,
-              particle.level(), particle.x0(), back_integration, distance);
+              particle.x0(), back_integration, distance);
       CHECK(distance < eps);
     }
   }
@@ -146,11 +150,11 @@ TEST_CASE("autonomous_particle_dg_backward_integration",
   // for (size_t y = 1; y < g.size(1) - 1; ++y) {
   //
   //  for (size_t x = 1; x < g.size(0) - 1; ++x) { x0s.push_back(g(x, y)); }
-  //10
+  // 10
   // autonomous_particles_test_backward_integation_distance(
   //    "dg", v, x0s, g.spacing(0), 0, 10, 0.1);
   autonomous_particles_test_backward_integation_distance("dg", v, vec{1.0, 0.5},
-                                                         0.01, 0, 10, 0.1);
+                                                         0.001, 0, 10, 0.1);
 }
 //==============================================================================
 TEST_CASE("autonomous_particle_saddle_vtk",
@@ -188,13 +192,14 @@ TEST_CASE("autonomous_particle_test_field_vtk",
 //------------------------------------------------------------------------------
 TEST_CASE("autonomous_particle_test_field_backward_integration",
           "[autonomous_particle][test_field][backward_integration]") {
-  //grid const                  g{linspace{-1.0, 1.0, 3}, linspace{-1.0, 1.0, 3}};
-  //std::vector<vec<double, 2>> x0s;
-  //for (size_t y = 1; y < g.size(1) - 1; ++y) {
+  // grid const                  g{linspace{-1.0, 1.0, 3}, linspace{-1.0, 1.0,
+  // 3}}; std::vector<vec<double, 2>> x0s; for (size_t y = 1; y < g.size(1) - 1;
+  // ++y) {
   //  for (size_t x = 1; x < g.size(0) - 1; ++x) { x0s.push_back(g(x, y)); }
   //}
-  //autonomous_particles_test_backward_integation_distance(
-  //    "test_field", autonomous_particles_test{}, x0s, g.spacing(0), 0, 3, 0.1);
+  // autonomous_particles_test_backward_integation_distance(
+  //    "test_field", autonomous_particles_test{}, x0s, g.spacing(0), 0, 3,
+  //    0.1);
   autonomous_particles_test_backward_integation_distance(
       "test_field", autonomous_particles_test{}, vec{0.0, 0.0}, 0.001, 0, 10,
       0.1);
