259df3d6e1d36c4c9eb333127aba7449fd4e56ab
Steve Wolligandt <steve@isg.cs.uni-magdeburg.de>
2020-07-22 08:57:06 +0000

    new style for swap_endianess.h

--- include/tatooine/swap_endianess.h
+++ include/tatooine/swap_endianess.h
@@ -1,14 +1,13 @@
-#ifndef __TATOOINE_SWAP_ENDIANESS_H__
-#define __TATOOINE_SWAP_ENDIANESS_H__
-
+#ifndef TATOOINE_SWAP_ENDIANESS_H
+#define TATOOINE_SWAP_ENDIANESS_H
+//==============================================================================
 #include <vector>
 //==============================================================================
 namespace tatooine {
 //==============================================================================
-
-template <typename data_t>
-data_t swap_endianess(data_t data) {
-  constexpr auto size = sizeof(data_t);
+template <typename Data>
+Data swap_endianess(Data data) {
+  constexpr auto size = sizeof(Data);
   using mem_t         = unsigned char *;
   auto mem            = reinterpret_cast<mem_t>(&data);
 
@@ -16,12 +15,10 @@ data_t swap_endianess(data_t data) {
   for (size_t i = 0; i < size / 2; i++) std::swap(mem[i], mem[size - 1 - i]);
   return data;
 }
-
 //------------------------------------------------------------------------------
-
-template <typename data_t>
-void swap_endianess(data_t *data, size_t n) {
-  constexpr size_t size = sizeof(data_t);
+template <typename Data>
+void swap_endianess(Data *data, size_t n) {
+  constexpr size_t size = sizeof(Data);
   using mem_t           = unsigned char *;
   for (size_t i = 0; i < n; ++i) {
     auto mem = reinterpret_cast<mem_t>(&data[i]);
@@ -29,16 +26,12 @@ void swap_endianess(data_t *data, size_t n) {
     for (size_t j = 0; j < size / 2; j++) std::swap(mem[j], mem[size - 1 - j]);
   }
 }
-
 //------------------------------------------------------------------------------
-
-template <typename data_t>
-void swap_endianess(std::vector<data_t> &data) {
+template <typename Data>
+void swap_endianess(std::vector<Data> &data) {
   swap_endianess(data.data(), data.size());
 }
-
 //==============================================================================
 }  // namespace tatooine
 //==============================================================================
-
 #endif
