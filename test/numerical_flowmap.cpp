6d142e2b138a5e4d5f7e56aeee6da9f191b88e34
Steve Wolligandt <steve@isg.cs.ovgu.de>
2020-10-06 15:34:29 +0200

    -

--- test/numerical_flowmap.cpp
+++ test/numerical_flowmap.cpp
@@ -2,7 +2,7 @@
 #include <tatooine/numerical_flowmap.h>
 #include <catch2/catch.hpp>
 //==============================================================================
-namespace tatooine::ode::vclibs::test {
+namespace tatooine::ode::test {
 //==============================================================================
 TEST_CASE("numerical_flowmap_doublegyre",
           "[vc][rk43][rungekutta43][ode][integrator][integration][flowmap][dg]["
@@ -20,6 +20,25 @@ TEST_CASE("numerical_flowmap_doublegyre",
   [[maybe_unused]] auto const xn1n5 = fm(vec{0.1, 0.1}, 0, -5);
 
   auto fmgrad = diff(fm);
+}
+//==============================================================================
+TEST_CASE("numerical_flowmap_internal_pointer",
+          "[vc][rk43][rungekutta43][ode][integrator][integration][flowmap][dg]["
+          "doublegyre][pointer]") {
+  numerical_flowmap_field_pointer<double, 2, ode::vclibs::rungekutta43,
+                                  interpolation::cubic> fm;
+  analytical::fields::numerical::doublegyre             v;
+  parent::vectorfield<double, 2>*                       polymorphic_field = &v;
+  fm.set_vectorfield(polymorphic_field);
+  [[maybe_unused]] auto const x010  = fm(vec{0.1, 0.1}, 0, 10);
+  [[maybe_unused]] auto const x015 = fm(vec{0.1, 0.1}, 0, 15);
+  [[maybe_unused]] auto const x0n5 = fm(vec{0.1, 0.1}, 0, -5);
+  [[maybe_unused]] auto const x110 = fm(vec{0.1, 0.1}, 1, 10);
+  [[maybe_unused]] auto const x115 = fm(vec{0.1, 0.1}, 1, 15);
+  [[maybe_unused]] auto const x1n5 = fm(vec{0.1, 0.1}, 1, -5);
+  [[maybe_unused]] auto const xn110 = fm(vec{0.1, 0.1}, 0, 10);
+  [[maybe_unused]] auto const xn115 = fm(vec{0.1, 0.1}, 0, 15);
+  [[maybe_unused]] auto const xn1n5 = fm(vec{0.1, 0.1}, 0, -5);
 }
 //==============================================================================
 }
