771789de6ae8999dcc978121ed46a4aea6bdc255
Steve Wolligandt <stevewolligandt@gmail.com>
2020-11-18 22:56:21 +0100

    started modernizing vtk_legacy part

--- include/tatooine/type_to_str.h
+++ include/tatooine/type_to_str.h
@@ -1,71 +1,69 @@
 #ifndef TATOOINE_TYPE_TO_STR_H
 #define TATOOINE_TYPE_TO_STR_H
-
 //==============================================================================
 namespace tatooine {
 //==============================================================================
-
 template <typename>
 struct type_to_str_false_type : std::false_type {};
-
+//------------------------------------------------------------------------------
 template <typename Data>
-constexpr std::string_view type_to_str() {
+constexpr auto type_to_str() -> std::string_view {
   static_assert(type_to_str_false_type<Data>::value, "unknown type");
   return "";
 }
-//-----------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 template <>
-constexpr std::string_view type_to_str<double>() {
+constexpr auto type_to_str<double>() -> std::string_view {
   return "double";
 }
-//-----------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 template <>
-constexpr std::string_view type_to_str<long double>() {
+constexpr auto type_to_str<long double>() -> std::string_view {
   return "long double";
 }
-//-----------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 template <>
-constexpr std::string_view type_to_str<float>() {
+constexpr auto type_to_str<float>() -> std::string_view {
   return "float";
 }
-//-----------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 template <>
-constexpr std::string_view type_to_str<int>() {
+constexpr auto type_to_str<int>() -> std::string_view {
   return "int";
 }
-//-----------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 template <>
-constexpr std::string_view type_to_str<unsigned int>() {
+constexpr auto type_to_str<unsigned int>() -> std::string_view {
   return "unsigned int";
 }
-//-----------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 template <>
-constexpr std::string_view type_to_str<long>() {
+constexpr auto type_to_str<long>() -> std::string_view {
   return "long";
 }
-//-----------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 template <>
-constexpr std::string_view type_to_str<unsigned long>() {
+constexpr auto type_to_str<unsigned long>() -> std::string_view {
   return "unsigned long";
 }
-//-----------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 template <>
-constexpr std::string_view type_to_str<long long>() {
+constexpr auto type_to_str<long long>() -> std::string_view {
   return "long long";
 }
-//-----------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 template <>
-constexpr std::string_view type_to_str<unsigned long long>() {
+constexpr auto type_to_str<unsigned long long>() -> std::string_view {
   return "unsigned long long";
 }
-//-----------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 template <>
-constexpr std::string_view type_to_str<char>() {
+constexpr auto type_to_str<char>() -> std::string_view {
   return "char";
 }
-//-----------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 template <>
-constexpr std::string_view type_to_str<unsigned char>() {
+constexpr auto type_to_str<unsigned char>() -> std::string_view {
   return "unsigned char";
 }
 //==============================================================================
