a213bcae46f17933fc26508bbed20024bba12bab
Steve Wolligandt <steve@isg.cs.ovgu.de>
2021-01-20 16:49:41 +0100

    -

--- mpi/fortran/c_interface.f90
+++ mpi/fortran/c_interface.f90
@@ -1,30 +1,32 @@
-module c_interface
-  !==============================================================================
-  ! These are the interfaces to the c functions
-  !==============================================================================
+!===============================================================================
+! These are the interfaces to the c functions
+!===============================================================================
+module tatooine_insitu_interface_mod
   interface
+    !---------------------------------------------------------------------------
     subroutine hello_world() bind(C, name="hello_world")
       implicit none
     end subroutine hello_world
-    !----------------------------------------------------------------------------
+    !---------------------------------------------------------------------------
     function add(a, b) result(c) bind(C, name="add")
       use iso_c_binding
       implicit none
       integer(c_int), intent(in), value:: a, b
       integer(c_int):: c
     end function add
-    !----------------------------------------------------------------------------
+    !---------------------------------------------------------------------------
     subroutine add_matrix(A, B, s1, s2)  bind(C, name="add_matrix")
       use iso_c_binding
       implicit none
-      integer(c_int), intent(in), dimension(:,:):: A, B
+      type(c_ptr), value:: A, B
       integer(c_int), intent(in), value:: s1, s2
     end subroutine add_matrix
-    !----------------------------------------------------------------------------
+    !---------------------------------------------------------------------------
     subroutine multi_arr(arr) bind(C, name="multi_arr")
       use iso_c_binding
       implicit none
-      integer(c_int), intent(in), dimension(:, :, :, :):: arr
+      type(c_ptr), value:: arr
     end subroutine multi_arr
+    !---------------------------------------------------------------------------
   end interface
-end module c_interface
+end module tatooine_insitu_interface_mod
