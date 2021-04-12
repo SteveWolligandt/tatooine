e933b668621c5cc69a38b4f89314b1f64d741c19
Steve Wolligandt <steve@isg.cs.ovgu.de>
2021-04-12 14:45:00 +0200

    -

--- flowexplorer/include/tatooine/flowexplorer/nodes/parallel_vectors.h
+++ flowexplorer/include/tatooine/flowexplorer/nodes/parallel_vectors.h
@@ -9,6 +9,8 @@
 namespace tatooine::flowexplorer::nodes {
 //==============================================================================
 struct parallel_vectors : renderable<parallel_vectors> {
+  std::array<GLfloat, 4>       m_line_color;
+  int                          m_line_width;
   ui::input_pin&               v_pin;
   ui::input_pin&               w_pin;
   ui::input_pin&               grid_pin;
@@ -18,12 +20,17 @@ struct parallel_vectors : renderable<parallel_vectors> {
   parallel_vectors(flowexplorer::scene& s);
   ~parallel_vectors() = default;
   //============================================================================
+  auto calculate() -> void;
   auto render(mat4f const& P, mat4f const& V) -> void override;
   auto on_pin_connected(ui::input_pin&, ui::output_pin&) -> void override;
+  auto draw_properties() -> bool override;
+  auto is_transparent() const -> bool override { return m_line_color[3] < 255; }
 };
 //==============================================================================
 }  // namespace tatooine::flowexplorer::nodes
 //==============================================================================
 TATOOINE_FLOWEXPLORER_REGISTER_RENDERABLE(
-    tatooine::flowexplorer::nodes::parallel_vectors)
+    tatooine::flowexplorer::nodes::parallel_vectors,
+    TATOOINE_REFLECTION_INSERT_METHOD(line_width, m_line_width),
+    TATOOINE_REFLECTION_INSERT_METHOD(line_color, m_line_color))
 #endif
