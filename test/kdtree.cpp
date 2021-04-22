13345906b579132ac49325be8becadc6819914c1
Steve Wolligandt <stevewolligandt@gmail.com>
2021-04-22 23:01:17 +0200

    3d kdtree writing

--- test/kdtree.cpp
+++ test/kdtree.cpp
@@ -17,5 +17,16 @@ TEST_CASE("kdtree_pointset_2d", "[kdtree][pointset][2d]") {
   hierarchy.write_vtk("kdtree2d.vtk");
 }
 //==============================================================================
+TEST_CASE("kdtree_pointset_3d", "[kdtree][pointset][3d]") {
+  pointset<real_t, 3> ps;
+  size_t const num_points = 101;
+  for (size_t i = 0; i < num_points; ++i) {
+    ps.insert_vertex(aabb3{vec3{0, 0, 0}, vec3{2, 1, 1}}.random_point());
+  }
+  kdtree              hierarchy{ps};
+  ps.write_vtk("kdtree3d_vertices.vtk");
+  hierarchy.write_vtk("kdtree3d.vtk");
+}
+//==============================================================================
 }  // namespace tatooine::test
 //==============================================================================
