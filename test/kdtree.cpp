88ffefe278fd3c3f3944d202092556a5fe5e5fd0
Steve Wolligandt <stevewolligandt@gmail.com>
2021-04-22 21:56:33 +0200

    2d kdtree working with vertices

--- test/kdtree.cpp
+++ test/kdtree.cpp
@@ -8,8 +8,9 @@ namespace tatooine::test {
 //==============================================================================
 TEST_CASE("kdtree_pointset_2d", "[kdtree][pointset][2d]") {
   pointset<real_t, 2> ps;
-  for (size_t i = 0; i < 100; ++i) {
-    ps.insert_vertex(aabb2{vec2{0, 0}, vec2{1, 1}}.random_point());
+  size_t const num_points = 101;
+  for (size_t i = 0; i < num_points; ++i) {
+    ps.insert_vertex(aabb2{vec2{0, 0}, vec2{2, 1}}.random_point());
   }
   kdtree              hierarchy{ps};
   ps.write_vtk("kdtree2d_vertices.vtk");
