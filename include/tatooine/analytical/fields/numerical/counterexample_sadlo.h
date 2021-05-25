07ee6d32ea5071afae641fb406bfbdb665b9c7fa
Steve Wolligandt <stevewolligandt@gmail.com>
2021-05-25 21:28:36 +0200

    solvers for cubic and quartic polynomials

--- include/tatooine/analytical/fields/numerical/counterexample_sadlo.h
+++ include/tatooine/analytical/fields/numerical/counterexample_sadlo.h
@@ -1,25 +1,23 @@
 #ifndef TATOOINE_ANALYTICAL_FIELDS_NUMERICAL_COUNTEREXAMPLE_SADLO_H
 #define TATOOINE_ANALYTICAL_FIELDS_NUMERICAL_COUNTEREXAMPLE_SADLO_H
 //==============================================================================
+#include <tatooine/field.h>
+
 #include <boost/math/constants/constants.hpp>
 #include <cmath>
-#include <tatooine/field.h>
 //==============================================================================
 namespace tatooine::analytical::fields::numerical {
 //==============================================================================
 template <typename Real>
-struct counterexample_sadlo
-    : field<counterexample_sadlo<Real>, Real, 2, 2> {
+struct counterexample_sadlo : vectorfield<counterexample_sadlo<Real>, Real, 2> {
   using this_t   = counterexample_sadlo<Real>;
-  using parent_t = field<this_t, Real, 2, 2>;
+  using parent_t = vectorfield<this_t, Real, 2>;
   using typename parent_t::pos_t;
   using typename parent_t::tensor_t;
-  using vec_t = vec<Real, 2>;
-
   //============================================================================
   constexpr counterexample_sadlo() noexcept {}
   //----------------------------------------------------------------------------
-  constexpr tensor_t evaluate(const pos_t& p, Real t) const {
+  constexpr auto evaluate(const pos_t& p, Real t) const -> tensor_t {
     const Real x    = p(0);
     const Real y    = p(1);
     const Real xx   = x * x;
@@ -30,28 +28,28 @@ struct counterexample_sadlo
                     r * (0.5 * y - 0.5 * std::sin(t) + std::cos(t))};
   }
 
-  struct bifurcationline_t{
+  struct bifurcationline_t {
     vec<Real, 2> at(Real t) const { return {std::cos(t), std::sin(t)}; }
     auto         operator()(Real t) const { return at(t); }
   };
 
   struct bifurcationline_spacetime_t {
-    vec<Real, 3> at(Real t) const {
-      return {std::cos(t), std::sin(t), t};
-    }
+    vec<Real, 3> at(Real t) const { return {std::cos(t), std::sin(t), t}; }
     auto         operator()(Real t) const { return at(t); }
   };
 
-  auto bifurcationline() const { return bifurcationline_t{};}
-  auto bifurcationline_spacetime() const { return bifurcationline_spacetime_t{};}
+  auto bifurcationline() const { return bifurcationline_t{}; }
+  auto bifurcationline_spacetime() const {
+    return bifurcationline_spacetime_t{};
+  }
 
-  constexpr bool in_domain(const pos_t& x, Real /*t*/) const {
+  constexpr auto in_domain(const pos_t& x, Real /*t*/) const -> bool {
     return length(x) <= 3;
   }
 };
 //==============================================================================
 counterexample_sadlo()->counterexample_sadlo<double>;
 //==============================================================================
-}  // namespace tatooine::numerical
+}  // namespace tatooine::analytical::fields::numerical
 //==============================================================================
 #endif
