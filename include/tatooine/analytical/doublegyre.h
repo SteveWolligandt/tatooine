62c50efabc7d12f670892ec1248a022b018ec510
Steve Wolligandt <steve@isg.cs.ovgu.de>
2021-08-03 11:05:59 +0200

    doc

---
+++ include/tatooine/analytical/doublegyre.h
@@ -0,0 +1,5 @@
+#ifndef TATOOINE_ANALYTICAL_FIELDS_DOUBLEGYRE_H
+#define TATOOINE_ANALYTICAL_FIELDS_DOUBLEGYRE_H
+#include <tatooine/analytical/fields/numerical/doublegyre.h>
+#include <tatooine/analytical/fields/symbolic/doublegyre.h>
+#endif
