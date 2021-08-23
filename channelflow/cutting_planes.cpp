f48b326d3e190daee110cdf508c3ebc8e58b020a
Steve Wolligandt <steve@isg.cs.ovgu.de>
2021-08-23 15:00:58 +0200

    cutting planes of channelflow

--- channelflow/cutting_planes.cpp
+++ channelflow/cutting_planes.cpp
@@ -1,82 +1,116 @@
 #include <tatooine/color_scales/magma.h>
 #include <tatooine/color_scales/viridis.h>
 #include <tatooine/field.h>
-#include <tatooine/rectilinear_grid.h>
 #include <tatooine/hdf5.h>
-#include <tatooine/line.h>
-#include <tatooine/rendering/direct_isosurface.h>
-#include <tatooine/rendering/perspective_camera.h>
+#include <tatooine/rectilinear_grid.h>
 //==============================================================================
 using namespace tatooine;
 //==============================================================================
-auto main() -> int {
-  auto const path = filesystem::path{"channelflow_velocity_y_154"};
-  if (filesystem::exists(path)) {
-    filesystem::remove_all(path);
+struct chengs_color_scale_t {
+  using real_t  = double;
+  using this_t  = chengs_color_scale_t;
+  using color_t = vec<real_t, 3>;
+  //==============================================================================
+  std::unique_ptr<real_t[]> m_data;
+  //==============================================================================
+  chengs_color_scale_t()
+      : m_data{new real_t[]{0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+                            0.0, 0.0, 0.0, 0.0, 0.0}} {}
+  //----------------------------------------------------------------------------
+  auto sample(real_t t) const {
+    if (t <= 0) {
+      return color_t{m_data[0], m_data[1], m_data[2]};
+    }
+    if (t >= 1) {
+      return color_t{m_data[4 * 3], m_data[4 * 3 + 1], m_data[4 * 3 + 2]};
+    }
+    t *= 4;
+    auto const i = static_cast<size_t>(std::floor(t));
+    t            = t - i;
+    return color_t{m_data[i * 3] * (1 - t) + m_data[(i + 1) * 3] * t,
+                   m_data[i * 3 + 1] * (1 - t) + m_data[(i + 1) * 3 + 1] * t,
+                   m_data[i * 3 + 2] * (1 - t) + m_data[(i + 1) * 3 + 2] * t};
   }
-  filesystem::create_directory(path);
-  // read full domain axes
-  std::cerr << "loading axes ...";
-  auto const axis0 = hdf5::file{"/home/vcuser/channel_flow/axis0.h5"}
-                         .dataset<double>("CartGrid/axis0")
-                         .read_as_vector();
-  auto const axis1 = hdf5::file{"/home/vcuser/channel_flow/axis1.h5"}
-                         .dataset<double>("CartGrid/axis1")
-                         .read_as_vector();
-  auto const axis2 = hdf5::file{"/home/vcuser/channel_flow/axis2.h5"}
-                         .dataset<double>("CartGrid/axis2")
-                         .read_as_vector();
-  std::cerr << "done!\n";
-
-  std::cerr << "creating grids ...";
-  rectilinear_grid full_domain{axis0, axis1, axis2};
-  full_domain.set_chunk_size_for_lazy_properties(256);
-  std::cerr << "full_domain:\n" << full_domain << '\n';
-
-  auto axis0_Q = axis0;
-  axis0_Q.pop_back();
-  rectilinear_grid full_domain_Q{axis0_Q, axis1, axis2};
-  full_domain_Q.set_chunk_size_for_lazy_properties(256);
-  std::cerr << "full_domain_Q:\n" << full_domain_Q << '\n';
-  std::cerr << "done!\n";
-
-  std::cerr << "creating files ...";
-  hdf5::file channelflow_154_file{
-      "/home/vcuser/channel_flow/dino_res_154000.h5"};
-  std::cerr << "done!\n";
-
-  std::cerr << "loading data ...";
-  auto& velocity_y_154 = full_domain.insert_vertex_property(
-      channelflow_154_file.dataset<double>("velocity/yvel"),
-      "velocity_y_154");
-  std::cerr << "done!\n";
-
-  std::cerr << "creating samplers ...";
-  auto velocity_y_154_sampler = velocity_y_154.linear_sampler();
-  std::cerr << "done!\n";
-
-  auto basis   = MatD<3, 2>{};
-  basis.col(0) = vec{0, axis1.back(), 0};
-  basis.col(1) = vec{0, 0, axis2.back()};
-
-  {
-    auto d = discretize(velocity_y_154_sampler, vec3{axis0[511], 0, 0}, basis,
-                        vec2{axis1.back(), axis2.back()}, size(axis1),
-                        size(axis2), "512", 0);
-    d.write_vtk("cutting_plane_512.vtk");
+  auto operator()(real_t const t) const { return sample(t); }
+};
+//==============================================================================
+auto main(int argc, char** argv) -> int {
+  auto const filepath = [&] {
+    if (argc > 1) {
+      return filesystem::path{argv[1]};
+    } else {
+      return filesystem::path{"/home/steve/channelflow/dino_res_186200.h5"};
+    }
+  }();
+  auto const out_path = filepath.filename().replace_extension("");
+  if (filesystem::exists(out_path)) {
+    filesystem::remove_all(out_path);
   }
+  filesystem::create_directory(out_path);
+  filesystem::create_directory(out_path / "viridis");
+  filesystem::create_directory(out_path / "cheng");
+  hdf5::file channelflow_file{filepath};
+  auto const axis0 =
+      channelflow_file.dataset<double>("CartGrid/axis0").read_as_vector();
+  auto const axis1 =
+      channelflow_file.dataset<double>("CartGrid/axis1").read_as_vector();
+  auto const axis2 =
+      channelflow_file.dataset<double>("CartGrid/axis2").read_as_vector();
 
-  {
-    auto d = discretize(velocity_y_154_sampler, vec3{axis0[255], 0, 0}, basis,
-                        vec2{axis1.back(), axis2.back()}, size(axis1),
-                        size(axis2), "256", 0);
-    d.write_vtk("cutting_plane_256.vtk");
-  }
+  std::cerr << "creating grids ...\n";
+  // rectilinear_grid full_domain{axis0, axis1, axis2};
+  std::cerr << "[ " << axis0[0] << ", " << axis0[1] << ", ..., " << axis0.back()
+            << " ], " << size(axis0) << "\n";
+  std::cerr << "[ " << axis1[0] << ", " << axis1[1] << ", ..., " << axis1.back()
+            << " ], " << size(axis1) << "\n";
+  std::cerr << "[ " << axis2[0] << ", " << axis2[1] << ", ..., " << axis2.back()
+            << " ], " << size(axis2) << "\n";
+  // full_domain.set_chunk_size_for_lazy_properties(4);
+  // full_domain.max_num_chunks_loaded(1000);
+  std::cerr << "done!\n";
 
-  {
-    auto d = discretize(velocity_y_154_sampler, vec3{axis0[127], 0, 0}, basis,
-                        vec2{axis1.back(), axis2.back()}, size(axis1),
-                        size(axis2), "128", 0);
-    d.write_vtk("cutting_plane_128.vtk");
+  // std::cerr << "loading data ...";
+  // auto& velocity_y = full_domain.insert_vertex_property(
+  //    channelflow_file.dataset<double>("velocity/yvel"), "velocity_y");
+  // std::cerr << "done!\n";
+
+  // std::cerr << "creating samplers ...";
+  // auto velocity_y_sampler = velocity_y.linear_sampler();
+  // std::cerr << "done!\n";
+
+  // auto basis   = MatD<3, 2>{};
+  // basis.col(0) = vec{0, axis1.back(), 0};
+  // basis.col(1) = vec{0, 0, axis2.back()};
+  // std::cerr << "basis:\n" <<basis << '\n';
+
+  auto const chengs_color_scale  = chengs_color_scale_t{};
+  auto const viridis_color_scale = color_scales::viridis{};
+  auto const plane_positions     = std::vector{512, 256, 128};
+  auto       cutting_plane_grid  = rectilinear_grid{axis1, axis2};
+  for (auto const plane_position : plane_positions) {
+    auto str          = std::to_string(plane_position);
+    auto filename_png = filesystem::path{"cutting_plane_" + str + ".png"};
+    auto filename_vtk = filesystem::path{"cutting_plane_" + str + ".vtk"};
+    std::cerr << "discretizing plane at " << str << "...\n";
+
+    auto& cutting_plane =
+        cutting_plane_grid.insert_contiguous_vertex_property<double>(str);
+
+    std::cerr << "  reading slice...\n";
+    channelflow_file.dataset<double>("velocity/yvel")
+        .read_chunk(std::vector<size_t>{size_t(plane_position - 1), 0, 0},
+                    std::vector<size_t>{1, size(axis1), size(axis2)},
+                    cutting_plane);
+    std::cerr << "  reading slice... done!\n";
+
+    //auto cutting_plane =
+    //    discretize(velocity_y_sampler, vec3{axis0[plane_position - 1], 0, 0},
+    //               basis, vec2{1, 1}, size(axis1), size(axis2), str, 0);
+    cutting_plane.write_png(out_path / "cheng" / filename_png,
+                            chengs_color_scale, 13, 27);
+    cutting_plane.write_png(out_path / "viridis" / filename_png,
+                            viridis_color_scale, 13, 27);
+    std::cerr << "discretizing plane at " << str << "... done!\n";
   }
+  cutting_plane_grid.write_vtk(out_path / "planes.vtk");
 }
--- channelflow/pv_mpi.cpp
+++ channelflow/pv_mpi.cpp
@@ -1,40 +1,42 @@
 #include <tatooine/analytical/fields/numerical/doublegyre.h>
-#include <tatooine/mpi/program.h>
 #include <tatooine/differentiated_field.h>
 #include <tatooine/hdf5.h>
 #include <tatooine/lazy_reader.h>
+#include <tatooine/mpi/program.h>
 #include <tatooine/parallel_vectors.h>
 #include <tatooine/spacetime_vectorfield.h>
-
-namespace tat = tatooine;
+//==============================================================================
+namespace tat    = tatooine;
 using doublegyre = tat::analytical::fields::numerical::doublegyre<tat::real_t>;
 using tat::linspace;
-
+//==============================================================================
 auto main(int argc, char** argv) -> int {
-  auto& mpi = tat::mpi::program::get(argc, argv);
+  auto&          mpi = tat::mpi::program::get(argc, argv);
   constexpr auto eps = 1e-5;
-  //------------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
   tat::hdf5::file channelflow_150_file{
       "/p/project/dnschannelfspp/project/channelf/data_hdf5/"
       "dino_res_150000.h5"};
   auto full_domain_x =
-    channelflow_150_file.dataset<double>("CartGrid/axis0").read_as_vector();
+      channelflow_150_file.dataset<double>("CartGrid/axis0").read_as_vector();
   auto full_domain_y =
-    channelflow_150_file.dataset<double>("CartGrid/axis1").read_as_vector();
+      channelflow_150_file.dataset<double>("CartGrid/axis1").read_as_vector();
   auto full_domain_z =
-    channelflow_150_file.dataset<double>("CartGrid/axis2").read_as_vector();
+      channelflow_150_file.dataset<double>("CartGrid/axis2").read_as_vector();
   full_domain_z.pop_back();
-  auto const xvel_150 = 
-    channelflow_150_file.dataset<double>("xvelocity/vel").read_lazy({2,2,2});
-  auto const yvel_150 = 
-    channelflow_150_file.dataset<double>("yvelocity/vel").read_lazy({2,2,2});
-  auto const zvel_150 = 
-    channelflow_150_file.dataset<double>("velocity/zvel").read_lazy({2,2,2});
-  tat::nonuniform_rectilinear_grid<double, 3> full_domain_grid{full_domain_x, full_domain_y, full_domain_z}; 
+  auto const xvel_150 = channelflow_150_file.dataset<double>("xvelocity/vel")
+                            .read_lazy({2, 2, 2});
+  auto const yvel_150 = channelflow_150_file.dataset<double>("yvelocity/vel")
+                            .read_lazy({2, 2, 2});
+  auto const zvel_150 = channelflow_150_file.dataset<double>("velocity/zvel")
+                            .read_lazy({2, 2, 2});
+  tat::nonuniform_rectilinear_grid<double, 3> full_domain_grid{
+      full_domain_x, full_domain_y, full_domain_z};
   full_domain_grid.update_diff_stencil_coefficients();
   mpi.init_communicator(full_domain_grid);
 
-  // local_grid covers all grid vertices exactly once distributed over all processes
+  // local_grid covers all grid vertices exactly once distributed over all
+  // processes
   auto const local_grid = mpi.local_grid(full_domain_grid);
   // halo_grid covers all grid cells exactly once distributed over all processes
   // with some redundant grid vertices.
@@ -59,15 +61,16 @@ auto main(int argc, char** argv) -> int {
     std::cout << "local grid\n" << local_grid << '\n';
     std::cout << "halo grid\n" << halo_grid;
   }
-  //------------------------------------------------------------------------------
-  //doublegyre dg;
-  //auto       v = spacetime(dg);
-  //auto       J = diff(v, 1e-10);
-  //auto       a = J * v;
+  //----------------------------------------------------------------------------
+  // doublegyre dg;
+  // auto       v = spacetime(dg);
+  // auto       J = diff(v, 1e-10);
+  // auto       a = J * v;
 
-  //auto const vortex_core_lines = parallel_vectors(v, a, halo_grid);
-  //write_vtk(vortex_core_lines, "pv_stdg_mpi_rank_" + std::to_string(mpi.rank()) + ".vtk");
-  //------------------------------------------------------------------------------
+  // auto const vortex_core_lines = parallel_vectors(v, a, halo_grid);
+  // write_vtk(vortex_core_lines, "pv_stdg_mpi_rank_" +
+  // std::to_string(mpi.rank()) + ".vtk");
+  //----------------------------------------------------------------------------
   auto calc_J_at = [&](auto ix, auto iy, auto iz) {
     ix += mpi.process_begin(0);
     iy += mpi.process_begin(1);
@@ -75,147 +78,147 @@ auto main(int argc, char** argv) -> int {
     auto J = tat::mat3::zeros();
     // calc x
     if (ix == 0) {
-      auto const stencil = full_domain_grid.diff_stencil_coefficients_0_p1_p2(0, ix);
-      J.col(0) = tat::vec3 {
-        xvel_150(ix    , iy, iz) * stencil[0] + 
-        xvel_150(ix + 1, iy, iz) * stencil[1] + 
-        xvel_150(ix + 2, iy, iz) * stencil[2],
-        yvel_150(ix    , iy, iz) * stencil[0] + 
-        yvel_150(ix + 1, iy, iz) * stencil[1] + 
-        yvel_150(ix + 2, iy, iz) * stencil[2],
-        zvel_150(ix    , iy, iz) * stencil[0] + 
-        zvel_150(ix + 1, iy, iz) * stencil[1] + 
-        zvel_150(ix + 2, iy, iz) * stencil[2] };
+      auto const stencil =
+          full_domain_grid.diff_stencil_coefficients_0_p1_p2(0, ix);
+      J.col(0) = tat::vec3{xvel_150(ix, iy, iz) * stencil[0] +
+                               xvel_150(ix + 1, iy, iz) * stencil[1] +
+                               xvel_150(ix + 2, iy, iz) * stencil[2],
+                           yvel_150(ix, iy, iz) * stencil[0] +
+                               yvel_150(ix + 1, iy, iz) * stencil[1] +
+                               yvel_150(ix + 2, iy, iz) * stencil[2],
+                           zvel_150(ix, iy, iz) * stencil[0] +
+                               zvel_150(ix + 1, iy, iz) * stencil[1] +
+                               zvel_150(ix + 2, iy, iz) * stencil[2]};
     } else if (ix == full_domain_grid.size(0) - 1) {
-      auto const stencil = full_domain_grid.diff_stencil_coefficients_n2_n1_0(0, ix);
-      J.col(0) = tat::vec3 {
-        xvel_150(ix - 2, iy, iz) * stencil[0] + 
-        xvel_150(ix - 1, iy, iz) * stencil[1] + 
-        xvel_150(ix    , iy, iz) * stencil[2], 
-        yvel_150(ix - 2, iy, iz) * stencil[0] + 
-        yvel_150(ix - 1, iy, iz) * stencil[1] + 
-        yvel_150(ix    , iy, iz) * stencil[2],
-        zvel_150(ix - 2, iy, iz) * stencil[0] + 
-        zvel_150(ix - 1, iy, iz) * stencil[1] + 
-        zvel_150(ix    , iy, iz) * stencil[2] };
+      auto const stencil =
+          full_domain_grid.diff_stencil_coefficients_n2_n1_0(0, ix);
+      J.col(0) = tat::vec3{xvel_150(ix - 2, iy, iz) * stencil[0] +
+                               xvel_150(ix - 1, iy, iz) * stencil[1] +
+                               xvel_150(ix, iy, iz) * stencil[2],
+                           yvel_150(ix - 2, iy, iz) * stencil[0] +
+                               yvel_150(ix - 1, iy, iz) * stencil[1] +
+                               yvel_150(ix, iy, iz) * stencil[2],
+                           zvel_150(ix - 2, iy, iz) * stencil[0] +
+                               zvel_150(ix - 1, iy, iz) * stencil[1] +
+                               zvel_150(ix, iy, iz) * stencil[2]};
     } else {
-      auto const stencil = full_domain_grid.diff_stencil_coefficients_n1_0_p1(0, ix);
-      J.col(0) = tat::vec3 {
-        xvel_150(ix - 1, iy, iz) * stencil[0] + 
-        xvel_150(ix    , iy, iz) * stencil[1] + 
-        xvel_150(ix + 1, iy, iz) * stencil[2], 
-        yvel_150(ix - 1, iy, iz) * stencil[0] + 
-        yvel_150(ix    , iy, iz) * stencil[1] + 
-        yvel_150(ix + 1, iy, iz) * stencil[2],
-        zvel_150(ix - 1, iy, iz) * stencil[0] + 
-        zvel_150(ix    , iy, iz) * stencil[1] + 
-        zvel_150(ix + 1, iy, iz) * stencil[2] };
+      auto const stencil =
+          full_domain_grid.diff_stencil_coefficients_n1_0_p1(0, ix);
+      J.col(0) = tat::vec3{xvel_150(ix - 1, iy, iz) * stencil[0] +
+                               xvel_150(ix, iy, iz) * stencil[1] +
+                               xvel_150(ix + 1, iy, iz) * stencil[2],
+                           yvel_150(ix - 1, iy, iz) * stencil[0] +
+                               yvel_150(ix, iy, iz) * stencil[1] +
+                               yvel_150(ix + 1, iy, iz) * stencil[2],
+                           zvel_150(ix - 1, iy, iz) * stencil[0] +
+                               zvel_150(ix, iy, iz) * stencil[1] +
+                               zvel_150(ix + 1, iy, iz) * stencil[2]};
     }
     // calc y
     if (iy == 0) {
-      auto const stencil =  full_domain_grid.diff_stencil_coefficients_0_p1_p2(1, iy);
-      J.col(1) = tat::vec3 {
-        xvel_150(ix, iy    , iz) * stencil[0] + 
-        xvel_150(ix, iy + 1, iz) * stencil[1] + 
-        xvel_150(ix, iy + 2, iz) * stencil[2], 
-        yvel_150(ix, iy    , iz) * stencil[0] + 
-        yvel_150(ix, iy + 1, iz) * stencil[1] + 
-        yvel_150(ix, iy + 2, iz) * stencil[2],
-        zvel_150(ix, iy    , iz) * stencil[0] + 
-        zvel_150(ix, iy + 1, iz) * stencil[1] + 
-        zvel_150(ix, iy + 2, iz) * stencil[2] };
+      auto const stencil =
+          full_domain_grid.diff_stencil_coefficients_0_p1_p2(1, iy);
+      J.col(1) = tat::vec3{xvel_150(ix, iy, iz) * stencil[0] +
+                               xvel_150(ix, iy + 1, iz) * stencil[1] +
+                               xvel_150(ix, iy + 2, iz) * stencil[2],
+                           yvel_150(ix, iy, iz) * stencil[0] +
+                               yvel_150(ix, iy + 1, iz) * stencil[1] +
+                               yvel_150(ix, iy + 2, iz) * stencil[2],
+                           zvel_150(ix, iy, iz) * stencil[0] +
+                               zvel_150(ix, iy + 1, iz) * stencil[1] +
+                               zvel_150(ix, iy + 2, iz) * stencil[2]};
     } else if (iy == full_domain_grid.size(1) - 1) {
-      auto const stencil =  full_domain_grid.diff_stencil_coefficients_n2_n1_0(1, iy);
-      J.col(1) = tat::vec3 {
-        yvel_150(ix, iy - 2, iz) * stencil[0] + 
-        yvel_150(ix, iy - 1, iz) * stencil[1] + 
-        yvel_150(ix, iy    , iz) * stencil[2], 
-        yvel_150(ix, iy - 2, iz) * stencil[0] + 
-        yvel_150(ix, iy - 1, iz) * stencil[1] + 
-        yvel_150(ix, iy    , iz) * stencil[2],
-        zvel_150(ix, iy - 2, iz) * stencil[0] + 
-        zvel_150(ix, iy - 1, iz) * stencil[1] + 
-        zvel_150(ix, iy    , iz) * stencil[2] };
+      auto const stencil =
+          full_domain_grid.diff_stencil_coefficients_n2_n1_0(1, iy);
+      J.col(1) = tat::vec3{yvel_150(ix, iy - 2, iz) * stencil[0] +
+                               yvel_150(ix, iy - 1, iz) * stencil[1] +
+                               yvel_150(ix, iy, iz) * stencil[2],
+                           yvel_150(ix, iy - 2, iz) * stencil[0] +
+                               yvel_150(ix, iy - 1, iz) * stencil[1] +
+                               yvel_150(ix, iy, iz) * stencil[2],
+                           zvel_150(ix, iy - 2, iz) * stencil[0] +
+                               zvel_150(ix, iy - 1, iz) * stencil[1] +
+                               zvel_150(ix, iy, iz) * stencil[2]};
     } else {
-      auto const stencil = full_domain_grid.diff_stencil_coefficients_n1_0_p1(1, iy);
-      J.col(1) = tat::vec3 {
-        zvel_150(ix, iy - 1, iz) * stencil[0] + 
-        zvel_150(ix, iy    , iz) * stencil[1] + 
-        zvel_150(ix, iy + 1, iz) * stencil[2], 
-        yvel_150(ix, iy - 1, iz) * stencil[0] + 
-        yvel_150(ix, iy    , iz) * stencil[1] + 
-        yvel_150(ix, iy + 1, iz) * stencil[2],
-        zvel_150(ix, iy - 1, iz) * stencil[0] + 
-        zvel_150(ix, iy    , iz) * stencil[1] + 
-        zvel_150(ix, iy + 1, iz) * stencil[2] };
+      auto const stencil =
+          full_domain_grid.diff_stencil_coefficients_n1_0_p1(1, iy);
+      J.col(1) = tat::vec3{zvel_150(ix, iy - 1, iz) * stencil[0] +
+                               zvel_150(ix, iy, iz) * stencil[1] +
+                               zvel_150(ix, iy + 1, iz) * stencil[2],
+                           yvel_150(ix, iy - 1, iz) * stencil[0] +
+                               yvel_150(ix, iy, iz) * stencil[1] +
+                               yvel_150(ix, iy + 1, iz) * stencil[2],
+                           zvel_150(ix, iy - 1, iz) * stencil[0] +
+                               zvel_150(ix, iy, iz) * stencil[1] +
+                               zvel_150(ix, iy + 1, iz) * stencil[2]};
     }
     // calc z
     if (iz == 0) {
-      auto const stencil = full_domain_grid.diff_stencil_coefficients_0_p1_p2(2, iz);
-      J.col(2) = tat::vec3 {
-        xvel_150(ix, iy, iz    ) * stencil[0] + 
-        xvel_150(ix, iy, iz + 1) * stencil[1] + 
-        xvel_150(ix, iy, iz + 2) * stencil[2], 
-        yvel_150(ix, iy, iz    ) * stencil[0] + 
-        yvel_150(ix, iy, iz + 1) * stencil[1] + 
-        yvel_150(ix, iy, iz + 2) * stencil[2],
-        zvel_150(ix, iy, iz    ) * stencil[0] + 
-        zvel_150(ix, iy, iz + 1) * stencil[1] + 
-        zvel_150(ix, iy, iz + 2) * stencil[2] };
+      auto const stencil =
+          full_domain_grid.diff_stencil_coefficients_0_p1_p2(2, iz);
+      J.col(2) = tat::vec3{xvel_150(ix, iy, iz) * stencil[0] +
+                               xvel_150(ix, iy, iz + 1) * stencil[1] +
+                               xvel_150(ix, iy, iz + 2) * stencil[2],
+                           yvel_150(ix, iy, iz) * stencil[0] +
+                               yvel_150(ix, iy, iz + 1) * stencil[1] +
+                               yvel_150(ix, iy, iz + 2) * stencil[2],
+                           zvel_150(ix, iy, iz) * stencil[0] +
+                               zvel_150(ix, iy, iz + 1) * stencil[1] +
+                               zvel_150(ix, iy, iz + 2) * stencil[2]};
     } else if (iz == full_domain_grid.size(2) - 1) {
-      auto const stencil =  full_domain_grid.diff_stencil_coefficients_n2_n1_0(2, iz);
-      J.col(2) = tat::vec3 {
-        yvel_150(ix, iy, iz - 2) * stencil[0] + 
-        yvel_150(ix, iy, iz - 1) * stencil[1] + 
-        yvel_150(ix, iy, iz    ) * stencil[2], 
-        yvel_150(ix, iy, iz - 2) * stencil[0] + 
-        yvel_150(ix, iy, iz - 1) * stencil[1] + 
-        yvel_150(ix, iy, iz    ) * stencil[2],
-        zvel_150(ix, iy, iz - 2) * stencil[0] + 
-        zvel_150(ix, iy, iz - 1) * stencil[1] + 
-        zvel_150(ix, iy, iz    ) * stencil[2] };
+      auto const stencil =
+          full_domain_grid.diff_stencil_coefficients_n2_n1_0(2, iz);
+      J.col(2) = tat::vec3{yvel_150(ix, iy, iz - 2) * stencil[0] +
+                               yvel_150(ix, iy, iz - 1) * stencil[1] +
+                               yvel_150(ix, iy, iz) * stencil[2],
+                           yvel_150(ix, iy, iz - 2) * stencil[0] +
+                               yvel_150(ix, iy, iz - 1) * stencil[1] +
+                               yvel_150(ix, iy, iz) * stencil[2],
+                           zvel_150(ix, iy, iz - 2) * stencil[0] +
+                               zvel_150(ix, iy, iz - 1) * stencil[1] +
+                               zvel_150(ix, iy, iz) * stencil[2]};
     } else {
-      auto const stencil = full_domain_grid.diff_stencil_coefficients_n1_0_p1(2, iz);
-      J.col(2) = tat::vec3 {
-        zvel_150(ix, iy, iz - 1) * stencil[0] + 
-        zvel_150(ix, iy, iz    ) * stencil[1] + 
-        zvel_150(ix, iy, iz + 1) * stencil[2], 
-        yvel_150(ix, iy, iz - 1) * stencil[0] + 
-        yvel_150(ix, iy, iz    ) * stencil[1] + 
-        yvel_150(ix, iy, iz + 1) * stencil[2],
-        zvel_150(ix, iy, iz - 1) * stencil[0] + 
-        zvel_150(ix, iy, iz    ) * stencil[1] + 
-        zvel_150(ix, iy, iz + 1) * stencil[2] };
+      auto const stencil =
+          full_domain_grid.diff_stencil_coefficients_n1_0_p1(2, iz);
+      J.col(2) = tat::vec3{zvel_150(ix, iy, iz - 1) * stencil[0] +
+                               zvel_150(ix, iy, iz) * stencil[1] +
+                               zvel_150(ix, iy, iz + 1) * stencil[2],
+                           yvel_150(ix, iy, iz - 1) * stencil[0] +
+                               yvel_150(ix, iy, iz) * stencil[1] +
+                               yvel_150(ix, iy, iz + 1) * stencil[2],
+                           zvel_150(ix, iy, iz - 1) * stencil[0] +
+                               zvel_150(ix, iy, iz) * stencil[1] +
+                               zvel_150(ix, iy, iz + 1) * stencil[2]};
     }
     return J;
   };
   //----------------------------------------------------------------------------
   auto const vortex_core_lines = tat::detail::calc_parallel_vectors<double>(
-    [&](auto ix, auto iy, auto iz, auto const& /*p*/) {
-      ix += mpi.process_begin(0);
-      iy += mpi.process_begin(1);
-      iz += mpi.process_begin(2);
-      return tat::vec3{xvel_150(ix, iy, iz),
-                       yvel_150(ix, iy, iz),
-                       zvel_150(ix, iy, iz)};
-    },
-    [&](auto ix, auto iy, auto iz, auto const& /*p*/) {
-      auto const J = calc_J_at(ix, iy, iz);
-      ix += mpi.process_begin(0);
-      iy += mpi.process_begin(1);
-      iz += mpi.process_begin(2);
-      tat::vec3 vel{xvel_150(ix, iy, iz),
-                    yvel_150(ix, iy, iz),
-                    zvel_150(ix, iy, iz)};
-      return J * vel;
-    },
-    halo_grid
-    //,[&](auto const& x) {
-      //auto const eig = eigenvalues(J_150_field(x, 0));
-      //return std::abs(eig(0).imag()) > 0 ||
-             //std::abs(eig(1).imag()) > 0 ||
-             //std::abs(eig(2).imag()) > 0;
-    //}
+      [&](auto ix, auto iy, auto iz, auto const& /*p*/) {
+        ix += mpi.process_begin(0);
+        iy += mpi.process_begin(1);
+        iz += mpi.process_begin(2);
+        return tat::vec3{xvel_150(ix, iy, iz), yvel_150(ix, iy, iz),
+                         zvel_150(ix, iy, iz)};
+      },
+      [&](auto ix, auto iy, auto iz, auto const& /*p*/) {
+        auto const J = calc_J_at(ix, iy, iz);
+        ix += mpi.process_begin(0);
+        iy += mpi.process_begin(1);
+        iz += mpi.process_begin(2);
+        tat::vec3 vel{xvel_150(ix, iy, iz), yvel_150(ix, iy, iz),
+                      zvel_150(ix, iy, iz)};
+        return J * vel;
+      },
+      halo_grid
+      //,[&](auto const& x) {
+      // auto const eig = eigenvalues(J_150_field(x, 0));
+      // return std::abs(eig(0).imag()) > 0 ||
+      // std::abs(eig(1).imag()) > 0 ||
+      // std::abs(eig(2).imag()) > 0;
+      //}
   );
-  write_vtk(vortex_core_lines, "vortex_core_lines_channelflow_150000_mpi_rank_" + std::to_string(mpi.rank()) + ".vtk");
+  write_vtk(vortex_core_lines,
+            "vortex_core_lines_channelflow_150000_mpi_rank_" +
+                std::to_string(mpi.rank()) + ".vtk");
 }
