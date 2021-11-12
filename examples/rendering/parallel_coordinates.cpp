3cf038ef93ebbf6e36e9e41e6cb9f884b3233d0b
Steve Wolligandt <steve@isg.cs.ovgu.de>
2021-11-12 13:58:11 +0100

    parallel coordinates

--- examples/rendering/parallel_coordinates.cpp
+++ examples/rendering/parallel_coordinates.cpp
@@ -4,106 +4,113 @@ using namespace tatooine;
 //==============================================================================
 namespace ImGui {
 //==============================================================================
-auto parallel_coordinates(const char*                             label,
-                          std::vector<std::string> const&         names,
-                          std::vector<std::vector<double>> const& data) -> int {
+auto parallel_coordinates(const char*                             imgui_label,
+                          std::vector<std::string> const&         labels,
+                          std::vector<std::vector<double>> const& data,
+                          std::vector<std::pair<double, double>>& ranges)
+    -> int {
   // visuals
-  size_t const smoothness = 64;  // curve smoothness: the higher number of
-                                 // segments, the smoother curve
-  size_t const curve_width = 4;  // main curved line width
-  size_t const line_width  = 1;  // handlers: small lines width
-  size_t const grab_radius = 6;  // handlers: circle radius
-  size_t const grab_border = 2;  // handlers: circle border width
+  size_t const grab_radius = 12;  // handlers: circle radius
 
-  const ImGuiStyle& Style    = GetStyle();
-  ImDrawList*       DrawList = GetWindowDrawList();
-  ImGuiWindow*      Window   = GetCurrentWindow();
-  if (Window->SkipItems) return false;
+  auto              active_bar = std::numeric_limits<std::size_t>::max();
+  const ImGuiStyle& Style      = GetStyle();
+  ImDrawList*       DrawList   = GetWindowDrawList();
+  ImGuiWindow*      Window     = GetCurrentWindow();
+  if (Window->SkipItems) {
+    return false;
+  }
 
   int changed = 0;
-  //Dummy(ImVec2(0, 3));
 
   // prepare canvas
-  float const width = 100;
-  auto const  canvas =
-      ImVec2{names.size() * width, ImGui::GetContentRegionAvail().y};
-  auto const bb = ImRect{Window->DC.CursorPos, Window->DC.CursorPos + canvas};
+  float const space_between_bars = 200;
+  float const bar_width          = 10;
+  auto const  canvas = ImVec2{(labels.size() - 1) * space_between_bars,
+                             ImGui::GetContentRegionAvail().y - 100};
+  auto const  bb = ImRect{Window->DC.CursorPos, Window->DC.CursorPos + canvas};
+  auto        extent = bb.Max - bb.Min;
+  {
+    char buf[128];
+    sprintf(buf, "0##%s", imgui_label);
+    auto handle_grabber_fixed = [&](std::size_t const          i,
+                                    std::pair<double, double>& range) {};
+    for (std::size_t i = 0; i < size(labels); ++i) {
+      {
+        ImVec2 pos = ImVec2(bb.Min.x + space_between_bars * i,
+                            bb.Min.y + ranges[i].first * extent.y);
+        SetCursorScreenPos(pos - ImVec2(grab_radius, grab_radius));
+        InvisibleButton((buf[0]++, buf),
+                        ImVec2(2 * grab_radius, 2 * grab_radius));
+        if (IsItemActive() || IsItemHovered()) {
+          active_bar = i;
+        }
+        if (IsItemActive() && IsMouseDragging(0)) {
+          ranges[i].first += GetIO().MouseDelta.y / canvas.y;
+          ranges[i].first =
+              std::max<float>(0.0f, std::min<float>(1.0f, ranges[i].first));
+          changed = true;
+        }
+      }
+      {
+        ImVec2 pos = ImVec2(bb.Min.x + space_between_bars * i,
+                            bb.Min.y + ranges[i].second * extent.y);
+        SetCursorScreenPos(pos - ImVec2(grab_radius, grab_radius));
+        InvisibleButton((buf[0]++, buf),
+                        ImVec2(2 * grab_radius, 2 * grab_radius));
+        if (IsItemActive() || IsItemHovered()) {
+          active_bar = i;
+        }
+        if (IsItemActive() && IsMouseDragging(0)) {
+          ranges[i].second += GetIO().MouseDelta.y / canvas.y;
+          ranges[i].second =
+              std::max<float>(0.0f, std::min<float>(1.0f, ranges[i].second));
+          changed = true;
+        }
+      }
+    }
+  }
+
   RenderFrame(bb.Min, bb.Max, GetColorU32(ImGuiCol_FrameBg, 1), true,
               Style.FrameRounding);
 
-  for (auto const& point:data) {
-    for (size_t i = 0; i < size(point)-1; ++i) {
-      DrawList->AddLine(
-          ImVec2(bb.Min.x + i * width, bb.Min.y + canvas.y * point[i]),
-          ImVec2(bb.Min.x + (i + 1) * width,
-                 bb.Min.y + canvas.y * point[i + 1]),
-          GetColorU32(ImGuiCol_TextDisabled));
+  auto const data_line_col =
+      ColorConvertFloat4ToU32(ImVec4{1.0f, 1.0f, 1.0f, 0.3f});
+  auto const data_line_outside_col =
+      ColorConvertFloat4ToU32(ImVec4{1.0f, 1.0f, 1.0f, 0.01f});
+
+  // draw data lines
+  for (auto const& point : data) {
+    bool inside = true;
+    for (size_t i = 0; i < size(point); ++i) {
+      if (point[i] < ranges[i].first || point[i] > ranges[i].second) {
+        inside = false;
+        break;
+      }
+    }
+    for (size_t i = 0; i < size(point) - 1; ++i) {
+      DrawList->AddLine(ImVec2(bb.Min.x + i * space_between_bars,
+                               bb.Min.y + extent.y * point[i]),
+                        ImVec2(bb.Min.x + (i + 1) * space_between_bars,
+                               bb.Min.y + extent.y * point[i + 1]),
+                        inside ? data_line_col : data_line_outside_col);
     }
   }
-  //for (int i = 0; i <= canvas.y; i += (canvas.y / 4)) {
-  //  DrawList->AddLine(ImVec2(bb.Min.x, bb.Min.y + i),
-  //                    ImVec2(bb.Max.x, bb.Min.y + i),
-  //                    GetColorU32(ImGuiCol_TextDisabled));
-  //}
 
-
-  {
-    char buf[128];
-    sprintf(buf, "0##%s", label);
-
-    // handle grabbers
-    auto handle_grabber_fixed = [&](float* v) {
-      ImVec2 pos = ImVec2(v[0], 1 - v[1]) * (bb.Max - bb.Min) + bb.Min;
-      SetCursorScreenPos(pos - ImVec2(grab_radius, grab_radius));
-      InvisibleButton((buf[0]++, buf),
-                      ImVec2(2 * grab_radius, 2 * grab_radius));
-      if (IsItemActive() || IsItemHovered()) {
-        SetTooltip("(%4.3f, %4.3f)", v[0], v[1]);
-      }
-      if (IsItemActive() && IsMouseDragging(0)) {
-        v[1] -= GetIO().MouseDelta.y / canvas.y;
-        v[1] = std::max<float>(0.0f, std::min<float>(1.0f, v[1]));
-        changed = true;
-      }
-    };
-    auto handle_grabber = [&](float* v) {
-      ImVec2 pos = ImVec2(v[0], 1 - v[1]) * (bb.Max - bb.Min) + bb.Min;
-      SetCursorScreenPos(pos - ImVec2(grab_radius, grab_radius));
-      InvisibleButton((buf[0]++, buf),
-                      ImVec2(2 * grab_radius, 2 * grab_radius));
-      if (IsItemActive() || IsItemHovered()) {
-        SetTooltip("(%4.3f, %4.3f)", v[0], v[1]);
-      }
-      if (IsItemActive() && IsMouseDragging(0)) {
-        v[0] += GetIO().MouseDelta.x / canvas.x;
-        v[1] -= GetIO().MouseDelta.y / canvas.y;
-        v[0]    = std::max<float>(0.0f, std::min<float>(1.0f, v[0]));
-        v[1]    = std::max<float>(0.0f, std::min<float>(1.0f, v[1]));
-        changed = true;
-      }
-    };
-
-
-    // draw lines and grabbers
-    float  luma = IsItemActive() || IsItemHovered() ? 0.5f : 1.0f;
-    ImVec4 cyan(0.00f, 0.75f, 1.00f, luma);
-    ImVec4 white(GetStyle().Colors[ImGuiCol_Text]);
-    //for (size_t i = 0; i < num_handles; ++i) {
-    //  ImVec2 p00 =
-    //      ImVec2(handles[i * 4], 1 - handles[i * 4 + 1]) * (bb.Max - bb.Min) +
-    //      bb.Min;
-    //  ImVec2 p01 = ImVec2(handles[i * 4 + 2], 1 - handles[i * 4 + 3]) *
-    //                   (bb.Max - bb.Min) +
-    //               bb.Min;
-    //  DrawList->AddLine(p00, p01, ImColor(white), line_width);
-    //  DrawList->AddCircleFilled(p00, grab_radius, ImColor(white));
-    //  DrawList->AddCircleFilled(p00, grab_radius - grab_border, ImColor(cyan));
-    //  DrawList->AddCircleFilled(p01, grab_radius, ImColor(white));
-    //  DrawList->AddCircleFilled(p01, grab_radius - grab_border, ImColor(cyan));
-    //}
-
-    // restore cursor pos
-    SetCursorScreenPos(ImVec2(bb.Min.x, bb.Max.y + grab_radius));  // :P
+  SetCursorScreenPos(ImVec2(bb.Min.x, bb.Max.y));
+  for (std::size_t i = 0; i < size(labels); ++i) {
+    float base   = bb.Min.y + ranges[i].first * extent.y;
+    float height = (ranges[i].second - ranges[i].first) * extent.y;
+    DrawList->AddRectFilled(
+        ImVec2(bb.Min.x + i * space_between_bars - bar_width, base),
+        ImVec2(bb.Min.x + i * space_between_bars + bar_width, base + height),
+        ColorConvertFloat4ToU32(
+            ImVec4{1.0f, 1.0f, 1.0f, active_bar == i ? 0.8f : 0.5f}));
+  }
+  SetCursorScreenPos(ImVec2(bb.Min.x, bb.Max.y + 10));
+  Text(labels.front().c_str());
+  for (std::size_t i = 1; i < size(labels); ++i) {
+    SameLine(i * space_between_bars);
+    Text(labels[i].c_str());
   }
 
   return changed;
@@ -113,16 +120,33 @@ auto parallel_coordinates(const char*                             label,
 //==============================================================================
 auto win = std::unique_ptr<rendering::first_person_window>{};
 //==============================================================================
-auto render_loop(auto const& dt) {
+std::vector<std::vector<double>>       data;
+std::vector<std::string>               labels;
+std::vector<std::pair<double, double>> ranges;
+auto                                   render_loop(auto const& dt) {
   gl::clear_color_depth_buffer();
-  ImGui::parallel_coordinates(
-      "##pc", std::vector<std::string>{"abc", "def"},
-      std::vector{std::vector{0.0, 1.0}, std::vector{0.5, 0.0},
-                  std::vector{1.0, 0.0}});
+  ImGui::parallel_coordinates("pc", labels, data, ranges);
 }
 //==============================================================================
 auto main() -> int {
   auto win = std::make_unique<rendering::first_person_window>(800, 600);
   gl::clear_color(255, 255, 255, 255);
+  auto rand = random::uniform{0.0, 1.0};
+  labels.emplace_back("x");
+  labels.emplace_back("y");
+  labels.emplace_back("z");
+  labels.emplace_back("1");
+  labels.emplace_back("2");
+  labels.emplace_back("3");
+  labels.emplace_back("4");
+  for (std::size_t i = 0; i < labels.size(); ++i) {
+    ranges.emplace_back(0, 1);
+  }
+  for (std::size_t j = 0; j < 1000; ++j) {
+    auto& col = data.emplace_back();
+    for (std::size_t i = 0; i < labels.size(); ++i) {
+      col.push_back(rand());
+    }
+  }
   win->render_loop([&](auto const& dt) { render_loop(dt); });
 }
