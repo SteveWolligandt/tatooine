6e6157724f7bb97fe2a542f6cb2fb6bd7a6dc9b0
Steve Wolligandt <steve@isg.cs.ovgu.de>
2021-11-29 17:10:35 +0100

    pv using less memory but not quite working

--- examples/parallel_vectors.cpp
+++ examples/parallel_vectors.cpp
@@ -6,8 +6,9 @@ using namespace tatooine;
 auto main() -> int {
   auto v = analytical::fields::numerical::abcflow{};
   write_vtk(
-      parallel_vectors(v, diff(v, 1e-10) * v, linspace{-3.0, 3.0 + 1e-6, 250},
-                       linspace{-3.0, 3.0 + 1e-5, 250},
-                       linspace{-3.0, 3.0 + 1e-4, 250}),
+      parallel_vectors(v, diff(v, 1e-10) * v,
+                       linspace{-3.0, 3.0 + 1e-6, 100},
+                       linspace{-3.0, 3.0 + 1e-5, 100},
+                       linspace{-3.0, 3.0 + 1e-4, 100}),
       "pv_abc.vtk");
 }
