b093ffdd414cb334631f26c168a3f082dfef4ab6
Steve Wolligandt <steve@isg.cs.ovgu.de>
2021-11-30 14:41:18 +0100

    pv working

--- examples/parallel_vectors.cpp
+++ examples/parallel_vectors.cpp
@@ -7,8 +7,8 @@ auto main() -> int {
   auto v = analytical::fields::numerical::abcflow{};
   write_vtk(
       parallel_vectors(v, diff(v, 1e-10) * v,
-                       linspace{-3.0, 3.0 + 1e-6, 100},
-                       linspace{-3.0, 3.0 + 1e-5, 100},
-                       linspace{-3.0, 3.0 + 1e-4, 100}),
+                       linspace{-3.0, 3.0 + 1e-6, 50},
+                       linspace{-3.0, 3.0 + 1e-5, 50},
+                       linspace{-3.0, 3.0 + 1e-4, 50}),
       "pv_abc.vtk");
 }
