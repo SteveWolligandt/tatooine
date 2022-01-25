dac4703fee27e797430d6a37e63e9f6b369b421f
Steve Wolligandt <steve@isg.cs.ovgu.de>
2022-01-25 12:36:51 +0100

    more doc

--- include/tatooine/detail/autonomous_particle/split_behavior.h
+++ include/tatooine/detail/autonomous_particle/split_behavior.h
@@ -4,6 +4,7 @@
 #include <tatooine/concepts.h>
 #include <tatooine/math.h>
 #include <tatooine/vec.h>
+
 #include <array>
 //==============================================================================
 namespace tatooine::detail::autonomous_particle {
@@ -11,6 +12,7 @@ namespace tatooine::detail::autonomous_particle {
 template <floating_point Real, std::size_t NumDimensions>
 struct split_behaviors;
 //==============================================================================
+/// See \ref autonomous_particle_page_split_behaviors_2d "Split Behavior in 2D".
 template <floating_point Real>
 struct split_behaviors<Real, 2> {
   static auto constexpr one            = Real(1);
@@ -28,39 +30,29 @@ struct split_behaviors<Real, 2> {
   /// \image html autonomous_particle/2splits.svg
   struct two_splits {
     static auto constexpr split_cond = sqrt2;
-    static constexpr auto radii = std::array{
-        vec_t{1 / sqrt2, 1 / sqrt2/sqrt2},
-        vec_t{1 / sqrt2, 1 / sqrt2/sqrt2}};
-    static constexpr auto offsets = std::array{
-        vec_t{0, sqrt2/2/sqrt2},
-        vec_t{0, -sqrt2/2/sqrt2}
-    };
+    static constexpr auto radii =
+        std::array{vec_t{1 / sqrt2, 1 / sqrt2 / sqrt2},
+                   vec_t{1 / sqrt2, 1 / sqrt2 / sqrt2}};
+    static constexpr auto offsets =
+        std::array{vec_t{0, sqrt2 / 2 / sqrt2}, vec_t{0, -sqrt2 / 2 / sqrt2}};
   };
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   /// \image html autonomous_particle/3splits.svg
   struct three_splits {
     static auto constexpr split_cond = Real(2);
-    static constexpr auto radii    = std::array{
-        vec_t{half, quarter},
-        vec_t{one, half},
-        vec_t{half, quarter}};
-    static constexpr auto offsets  = std::array{
-        vec_t{0, -three_quarters},
-        vec_t{0, 0},
-        vec_t{0, three_quarters}};
+    static constexpr auto radii      = std::array{
+        vec_t{half, quarter}, vec_t{one, half}, vec_t{half, quarter}};
+    static constexpr auto offsets = std::array{
+        vec_t{0, -three_quarters}, vec_t{0, 0}, vec_t{0, three_quarters}};
   };
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   /// \image html autonomous_particle/3splits_alternative.svg
   struct three_in_square_splits {
     static auto constexpr split_cond = Real(3);
-    static constexpr auto radii    = std::array{
-        vec_t{one, third},
-        vec_t{one, third},
-        vec_t{one, third}};
-    static constexpr auto offsets = std::array{
-        vec_t{0, -two_thirds},
-        vec_t{0, 0},
-        vec_t{0, two_thirds}};
+    static constexpr auto radii =
+        std::array{vec_t{one, third}, vec_t{one, third}, vec_t{one, third}};
+    static constexpr auto offsets =
+        std::array{vec_t{0, -two_thirds}, vec_t{0, 0}, vec_t{0, two_thirds}};
   };
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   /// \image html autonomous_particle/5splits.svg
@@ -76,18 +68,12 @@ struct split_behaviors<Real, 2> {
 
     static auto constexpr split_cond = r1;
     static auto constexpr radii      = std::array{
-        vec_t{r3, r3 / r1},
-        vec_t{r4, r4 / r1},
-        vec_t{r2, r2 / r1},
-        vec_t{r4, r4 / r1},
-        vec_t{r3, r3 / r1},
+        vec_t{r3, r3 / r1}, vec_t{r4, r4 / r1}, vec_t{r2, r2 / r1},
+        vec_t{r4, r4 / r1}, vec_t{r3, r3 / r1},
     };
     static auto constexpr offsets = std::array{
-        vec_t{0, x3 / r1},
-        vec_t{0, x4 / r1},
-        vec_t{0, 0},
-        vec_t{0, -x4 / r1},
-        vec_t{0, -x3 / r1},
+        vec_t{0, x3 / r1},  vec_t{0, x4 / r1},  vec_t{0, 0},
+        vec_t{0, -x4 / r1}, vec_t{0, -x3 / r1},
     };
   };
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -110,29 +96,21 @@ struct split_behaviors<Real, 2> {
 
     static auto constexpr split_cond = r01 / r02;
 
-    static auto constexpr radii    = std::array{
-        vec_t{r1, r1/r01},
-        vec_t{r2, r2/r01},
-        vec_t{r2, r2/r01},
-        vec_t{r4, r4/r01},
-        vec_t{r4, r4/r01},
-        vec_t{r4, r4/r01},
-        vec_t{r4, r4/r01},
+    static auto constexpr radii = std::array{
+        vec_t{r1, r1 / r01}, vec_t{r2, r2 / r01}, vec_t{r2, r2 / r01},
+        vec_t{r4, r4 / r01}, vec_t{r4, r4 / r01}, vec_t{r4, r4 / r01},
+        vec_t{r4, r4 / r01},
     };
-    static auto constexpr offsets  = std::array{
-        vec_t{0, 0},
-        vec_t{ y2,  x2/r01},
-        vec_t{ y2, -x2/r01},
-        vec_t{ y4,  x4/r01},
-        vec_t{ y4, -x4/r01},
-        vec_t{-y4,  x4/r01},
-        vec_t{-y4, -x4/r01},
+    static auto constexpr offsets = std::array{
+        vec_t{0, 0},           vec_t{y2, x2 / r01},  vec_t{y2, -x2 / r01},
+        vec_t{y4, x4 / r01},   vec_t{y4, -x4 / r01}, vec_t{-y4, x4 / r01},
+        vec_t{-y4, -x4 / r01},
     };
   };
   //============================================================================
   /// \image html autonomous_particle/7splits.svg
   struct seven_splits {
-    static auto constexpr rr = Real(4.493959210);
+    static auto constexpr rr  = Real(4.493959210);
     static auto constexpr rr1 = Real(0.9009688678);
     static auto constexpr rr2 = Real(0.6234898004);
 
@@ -158,22 +136,24 @@ struct split_behaviors<Real, 2> {
     };
   };
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  struct centered_four {
-    static auto constexpr x5       = Real(0.4830517593887872);
-    static auto constexpr split_cond = Real(2);
-    static auto constexpr radii =
-        std::array{vec_t{x5, x5 / 2},
-                   vec_t{x5, x5 / 2},
-                   vec_t{x5, x5 / 2},
-                   vec_t{x5, x5 / 2},
-                   vec_t{real_t(1) / real_t(2), real_t(1) / real_t(4)},
-                   vec_t{real_t(1) / real_t(2), real_t(1) / real_t(4)}};
-    static auto constexpr offsets = std::array{
-        vec_t{-x5, -x5 / 2}, vec_t{x5, -x5 / 2},      vec_t{-x5, x5 / 2},
-        vec_t{x5, x5 / 2},   vec_t{0, real_t(3) / 4}, vec_t{0, -real_t(3) / 4}};
-  };
+  // struct centered_four {
+  //  static auto constexpr x5       = Real(0.4830517593887872);
+  //  static auto constexpr split_cond = Real(2);
+  //  static auto constexpr radii =
+  //      std::array{vec_t{x5, x5 / 2},
+  //                 vec_t{x5, x5 / 2},
+  //                 vec_t{x5, x5 / 2},
+  //                 vec_t{x5, x5 / 2},
+  //                 vec_t{real_t(1) / real_t(2), real_t(1) / real_t(4)},
+  //                 vec_t{real_t(1) / real_t(2), real_t(1) / real_t(4)}};
+  //  static auto constexpr offsets = std::array{
+  //      vec_t{-x5, -x5 / 2}, vec_t{x5, -x5 / 2},      vec_t{-x5, x5 / 2},
+  //      vec_t{x5, x5 / 2},   vec_t{0, real_t(3) / 4}, vec_t{0, -real_t(3) /
+  //      4}};
+  //};
 };
 //==============================================================================
+/// See \ref autonomous_particle_page_split_behaviors_3d "Split Behavior in 3D".
 template <floating_point Real>
 struct split_behaviors<Real, 3> {
   static auto constexpr half           = 1 / Real(2);
