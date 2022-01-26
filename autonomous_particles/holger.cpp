fc8b4d00e591d87697d3bb0087f53b205e963482
Steve Wolligandt <steve@isg.cs.ovgu.de>
2022-01-26 14:12:10 +0100

    -

--- autonomous_particles/holger.cpp
+++ autonomous_particles/holger.cpp
@@ -15,27 +15,27 @@ auto doublegyre_example() {
       autonomous_particle2::advect_with_three_splits(flowmap(v), 0.01, 0, 3,
                                                      initial_grid);
 
-  auto advected_discretizations = std::vector<line2>{};
-  auto initial_discretizations  = std::vector<line2>{};
-  auto advected_circle_discretizations  = std::vector<line2>{};
+  auto advected_discretizations        = std::vector<line2>{};
+  auto initial_discretizations         = std::vector<line2>{};
+  auto advected_circle_discretizations = std::vector<line2>{};
   auto initial_circle_discretizations  = std::vector<line2>{};
   advected_discretizations.reserve(size(advected_particles));
   initial_discretizations.reserve(size(advected_particles));
   advected_circle_discretizations.reserve(size(advected_particles));
   initial_circle_discretizations.reserve(size(advected_particles));
 
-  auto advected_discretized = [](auto const& p) {
-    return discretize(p, 64);
+  auto discretized = [](auto const& ell) {
+    return discretize(ell, 64);
   };
-  auto initial_discretized  = [](auto const& p) {
-    return discretize(p.initial_ellipse(), 64);
+  auto as_initial  = [](auto const& p) {
+    return p.initial_ellipse();
   };
   auto advected_circle_discretized = [](auto const& p) {
     auto ell = p.initial_ellipse();
     auto const [axes, lengths] = ell.main_axes();
     if (std::abs(lengths(0) - lengths(1)) > 1e-10) {
-      //ell.S() = axes * diag(vec{lengths(0), lengths(0)});
-      ell.S() = mat2::eye() * diag(vec{lengths(0), lengths(0)});
+      ell.S() = axes * diag(vec{lengths(0), lengths(0)}) * transposed(axes);
+      //ell.S() = mat2::eye() * diag(vec{lengths(0), lengths(0)});
     }
     ell.S()      = ell.S() * p.nabla_phi();
     ell.center() = p.center();
@@ -44,19 +44,33 @@ auto doublegyre_example() {
   auto initial_circle_discretized  = [](auto const& p) {
     auto ell = p.initial_ellipse();
     auto const [axes, lengths] = ell.main_axes();
-    ell.S() = axes * diag(vec{lengths(0), lengths(0)});
+    ell.S() = axes * diag(vec{lengths(0), lengths(0)}) * transposed(axes);
     return discretize(ell, 64);
   };
 
   using namespace std::ranges;
-  copy(advected_particles | views::transform(advected_discretized),
+  copy(advected_particles
+       | views::transform(discretized),
        std::back_inserter(advected_discretizations));
-  copy(advected_particles | views::transform(initial_discretized),
+  copy(advected_particles
+       | views::transform(as_initial)
+       | views::transform(discretized),
        std::back_inserter(initial_discretizations));
-  copy(advected_particles | views::transform(advected_circle_discretized),
+  copy(advected_particles
+       | views::transform(advected_circle_discretized),
        std::back_inserter(advected_circle_discretizations));
-  copy(advected_particles | views::transform(initial_circle_discretized),
+  copy(advected_particles
+       | views::transform(initial_circle_discretized),
        std::back_inserter(initial_circle_discretizations));
+
+  auto centers = pointset2{};
+  auto& dir0  = centers.vec2_vertex_property("dir0");
+  auto& dir1  = centers.vec2_vertex_property("dir1");
+  for (auto const& p : advected_particles) {
+    auto v = centers.insert_vertex(p.center());
+    dir0[v] = p.nabla_phi().col(0);
+    dir1[v] = p.nabla_phi().col(1);
+  }
 
   write(advected_discretizations, "holger_doublegyre_ellipses1.vtk");
   write(initial_discretizations, "holger_doublegyre_ellipses0.vtk");
@@ -64,6 +78,7 @@ auto doublegyre_example() {
         "holger_doublegyre_ellipses1_circle.vtk");
   write(initial_circle_discretizations,
         "holger_doublegyre_ellipses0_circle.vtk");
+  centers.write("holger_directions.vtk");
 }
 //==============================================================================
 auto main() -> int {
---
+++ include/tatooine/detail/pointset/vertex_container.h
@@ -0,0 +1,110 @@
+#ifndef TATOOINE_DETAIL_POINTSET_VERTEX_CONTAINER_H
+#define TATOOINE_DETAIL_POINTSET_VERTEX_CONTAINER_H
+//==============================================================================
+namespace tatooine::detail::pointset {
+//==============================================================================
+template <typename Real, std::size_t NumDimensions>
+struct vertex_container {
+  using pointset_t      = tatooine::pointset<Real, NumDimensions>;
+  using vertex_handle_t = typename pointset_t::vertex_handle;
+  struct iterator : iterator_facade<iterator> {
+    struct sentinel_type {};
+    iterator() = default;
+    iterator(vertex_handle_t const vh, pointset_t const* ps)
+        : m_vh{vh}, m_ps{ps} {}
+    iterator(iterator const& other) : m_vh{other.m_vh}, m_ps{other.m_ps} {}
+
+   private:
+    vertex_handle_t   m_vh{};
+    pointset_t const* m_ps = nullptr;
+
+   public:
+    constexpr auto increment() {
+      do {
+        ++m_vh;
+      } while (!m_ps->is_valid(m_vh));
+    }
+    constexpr auto decrement() {
+      do {
+        --m_vh;
+      } while (!m_ps->is_valid(m_vh));
+    }
+
+    [[nodiscard]] constexpr auto equal(iterator const& other) const {
+      return m_vh == other.m_vh;
+    }
+    [[nodiscard]] auto dereference() const { return m_vh; }
+
+    constexpr auto at_end() const {
+      return m_vh.index() == m_ps->vertex_position_data().size();
+    }
+  };
+  //==========================================================================
+ private:
+  pointset_t const* m_pointset;
+
+ public:
+  vertex_container(pointset_t const* ps) : m_pointset{ps} {}
+  vertex_container(vertex_container const&)     = default;
+  vertex_container(vertex_container&&) noexcept = default;
+  auto operator=(vertex_container const&) -> vertex_container& = default;
+  auto operator=(vertex_container&&) noexcept -> vertex_container& = default;
+  ~vertex_container()                                              = default;
+  //==========================================================================
+  auto begin() const {
+    iterator vi{vertex_handle_t{0}, m_pointset};
+    if (!m_pointset->is_valid(*vi)) {
+      ++vi;
+    }
+    return vi;
+  }
+  //--------------------------------------------------------------------------
+  static constexpr auto end() { return typename iterator::sentinel_type{}; }
+  //--------------------------------------------------------------------------
+  auto size() const {
+    return m_pointset->vertex_position_data().size() -
+           m_pointset->invalid_vertices().size();
+  }
+  auto data_container() const -> auto const& {
+    return m_pointset->vertex_position_data();
+  }
+  auto data() const { return data_container().data(); }
+  auto operator[](std::size_t const i) const {
+    return m_pointset->at(vertex_handle_t{i});
+  }
+  auto operator[](std::size_t const i) {
+    return m_pointset->at(vertex_handle_t{i});
+  }
+  auto operator[](vertex_handle_t const i) const { return m_pointset->at(i); }
+  auto operator[](vertex_handle_t const i) { return m_pointset->at(i); }
+  auto at(std::size_t const i) const {
+    return m_pointset->at(vertex_handle_t{i});
+  }
+  auto at(std::size_t const i) { return m_pointset->at(vertex_handle_t{i}); }
+  auto at(vertex_handle_t const i) const { return m_pointset->at(i); }
+  auto at(vertex_handle_t const i) { return m_pointset->at(i); }
+};
+//------------------------------------------------------------------------------
+template <typename Real, std::size_t NumDimensions>
+auto begin(vertex_container<Real, NumDimensions> verts) {
+  return verts.begin();
+}
+//------------------------------------------------------------------------------
+template <typename Real, std::size_t NumDimensions>
+auto end(vertex_container<Real, NumDimensions> verts) {
+  return verts.end();
+}
+//------------------------------------------------------------------------------
+template <typename Real, std::size_t NumDimensions>
+auto size(vertex_container<Real, NumDimensions> verts) {
+  return verts.size();
+}
+//==============================================================================
+}  // namespace tatooine::detail::pointset
+//==============================================================================
+template <typename Real, std::size_t NumDimensions>
+inline constexpr bool std::ranges::enable_borrowed_range<
+    typename tatooine::detail::pointset::vertex_container<Real,
+                                                          NumDimensions>> =
+    true;
+#endif
