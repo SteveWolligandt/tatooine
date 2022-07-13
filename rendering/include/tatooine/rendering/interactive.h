d1db636e6e732b412f40ef2cd5ace1a80509e344
Steve Wolligandt <steve@isg.cs.ovgu.de>
2022-07-13 09:42:51 +0200

    vclibs rk43 wrapper only stores options

--- rendering/include/tatooine/rendering/interactive.h
+++ rendering/include/tatooine/rendering/interactive.h
@@ -2,11 +2,11 @@
 #define TATOOINE_RENDERING_INTERACTIVE_H
 //==============================================================================
 #include <tatooine/rendering/first_person_window.h>
+#include <tatooine/rendering/interactive/edgeset2.h>
 #include <tatooine/rendering/interactive/ellipse.h>
 #include <tatooine/rendering/interactive/pointset.h>
 #include <tatooine/rendering/interactive/rectilinear_grid.h>
 #include <tatooine/rendering/interactive/unstructured_triangular_grid2.h>
-#include <tatooine/rendering/interactive/edgeset2.h>
 #include <tatooine/type_set.h>
 //==============================================================================
 namespace tatooine::rendering::interactive {
@@ -64,7 +64,7 @@ auto show(std::index_sequence<Is...> /*seq*/, Renderables&&... renderables) {
   using namespace detail;
   using renderer_type_set = type_set<renderer<std::decay_t<Renderables>>...>;
 
-  auto& window = detail::window::get();
+  auto&      window             = detail::window::get();
   auto const max_num_dimensions = tatooine::max([&] {
     using renderable_t = std::decay_t<decltype(renderables)>;
     if constexpr (range<renderable_t>) {
@@ -98,6 +98,10 @@ auto show(std::index_sequence<Is...> /*seq*/, Renderables&&... renderables) {
 
   gl::clear_color(255, 255, 255, 255);
 
+  auto& io     = ImGui::GetIO();
+  auto  roboto = io.Fonts->AddFontFromFileTTF(
+       "/home/steve/libs/tatooine2/resources/fonts/Roboto-Regular.ttf", 25);
+  auto enable_renderer = std::array{((void)renderables, true)...};
   auto renderers = std::tuple{[&](auto&& renderable) {
     using type         = decltype(renderable);
     using decayed_type = std::decay_t<type>;
@@ -126,43 +130,49 @@ auto show(std::index_sequence<Is...> /*seq*/, Renderables&&... renderables) {
 
   window.add_button_released_event([&](gl::button b) {
     foreach_renderer([&](auto& renderer, auto& renderable, auto i) {
-      if constexpr (requires { renderer.on_button_released(b); }) {
-        renderer.on_button_released(b);
-      }
-      if constexpr (requires {
-                      renderer.on_button_released(
-                          b, window.camera_controller().active_camera());
-                    }) {
-        renderer.on_button_released(b,
-                                   window.camera_controller().active_camera());
+      if (enable_renderer[i]) {
+        if constexpr (requires { renderer.on_button_released(b); }) {
+          renderer.on_button_released(b);
+        }
+        if constexpr (requires {
+                        renderer.on_button_released(
+                            b, window.camera_controller().active_camera());
+                      }) {
+          renderer.on_button_released(
+              b, window.camera_controller().active_camera());
+        }
       }
     });
   });
   window.add_button_pressed_event([&](gl::button b) {
     foreach_renderer([&](auto& renderer, auto& renderable, auto i) {
-      if constexpr (requires { renderer.on_button_pressed(b); }) {
-        renderer.on_button_pressed(b);
-      }
-      if constexpr (requires {
-                      renderer.on_button_pressed(
-                          b, window.camera_controller().active_camera());
-                    }) {
-        renderer.on_button_pressed(b,
-                                   window.camera_controller().active_camera());
+      if (enable_renderer[i]) {
+        if constexpr (requires { renderer.on_button_pressed(b); }) {
+          renderer.on_button_pressed(b);
+        }
+        if constexpr (requires {
+                        renderer.on_button_pressed(
+                            b, window.camera_controller().active_camera());
+                      }) {
+          renderer.on_button_pressed(
+              b, window.camera_controller().active_camera());
+        }
       }
     });
   });
   window.add_cursor_moved_event([&](double const x, double const y) {
     foreach_renderer([&](auto& renderer, auto& renderable, auto i) {
-      if constexpr (requires { renderer.on_cursor_moved(x, y); }) {
-        renderer.on_cursor_moved(x, y);
-      }
-      if constexpr (requires {
-                      renderer.on_cursor_moved(
-                          x, y, window.camera_controller().active_camera());
-                    }) {
-        renderer.on_cursor_moved(x, y,
-                                 window.camera_controller().active_camera());
+      if (enable_renderer[i]) {
+        if constexpr (requires { renderer.on_cursor_moved(x, y); }) {
+          renderer.on_cursor_moved(x, y);
+        }
+        if constexpr (requires {
+                        renderer.on_cursor_moved(
+                            x, y, window.camera_controller().active_camera());
+                      }) {
+          renderer.on_cursor_moved(x, y,
+                                   window.camera_controller().active_camera());
+        }
       }
     });
   });
@@ -173,59 +183,65 @@ auto show(std::index_sequence<Is...> /*seq*/, Renderables&&... renderables) {
 
     gl::clear_color_depth_buffer();
     ImGui::Begin("Properties");
+    //ImGui::PushFont(roboto);
     foreach_renderer([&](auto& renderer, auto& renderable, auto i) {
-      if constexpr (requires { renderer.update(dt); }) {
-        renderer.update(dt);
-      }
-      if constexpr (requires { renderer.update(dt, renderable); }) {
-        renderer.update(dt, renderable);
-      }
-      if constexpr (requires {
-                      renderer.update(
-                          dt, renderable,
+      if (enable_renderer[i]) {
+        if constexpr (requires { renderer.update(dt); }) {
+          renderer.update(dt);
+        }
+        if constexpr (requires { renderer.update(dt, renderable); }) {
+          renderer.update(dt, renderable);
+        }
+        if constexpr (requires {
+                        renderer.update(
+                            dt, renderable,
+                            window.camera_controller().active_camera());
+                      }) {
+          renderer.update(dt, renderable,
                           window.camera_controller().active_camera());
-                    }) {
-        renderer.update(dt, renderable,
-                        window.camera_controller().active_camera());
+        }
+        if constexpr (requires { renderer.render(); }) {
+          renderer.render();
+        }
+        if constexpr (requires { renderer.render(renderable); }) {
+          renderer.render(renderable);
+        }
+        if constexpr (requires {
+                        renderer.render(
+                            renderable,
+                            window.camera_controller().active_camera());
+                      }) {
+          renderer.render(renderable,
+                          window.camera_controller().active_camera());
+        }
       }
 
+      ImGui::PushID(i);
+      ImGui::ToggleButton("##enable", &enable_renderer[i]);
       if constexpr (requires { renderer.properties(renderable); }) {
-        ImGui::PushID(i);
         ImGui::BeginGroup();
         renderer.properties(renderable);
         ImGui::EndGroup();
-        ImGui::PopID();
       }
       if constexpr (requires { renderer.properties(); }) {
-        ImGui::PushID(i);
         ImGui::BeginGroup();
         renderer.properties();
         ImGui::EndGroup();
-        ImGui::PopID();
       }
-      if constexpr (requires { renderer.render(); }) {
-        renderer.render();
-      }
-      if constexpr (requires { renderer.render(renderable); }) {
-        renderer.render(renderable);
-      }
-      if constexpr (requires {
-                      renderer.render(
-                          renderable,
-                          window.camera_controller().active_camera());
-                    }) {
-        renderer.render(renderable, window.camera_controller().active_camera());
-      }
+      ImGui::PopID();
     });
+    //ImGui::PopFont();
     ImGui::End();
     gl::clear_depth_buffer();
     // In this late render area you can use custom cameras.
     foreach_renderer([&](auto& renderer, auto& renderable, auto i) {
-      if constexpr (requires { renderer.late_render(); }) {
-        renderer.late_render();
-      }
-      if constexpr (requires { renderer.late_render(renderable); }) {
-        renderer.late_render(renderable);
+      if (enable_renderer[i]) {
+        if constexpr (requires { renderer.late_render(); }) {
+          renderer.late_render();
+        }
+        if constexpr (requires { renderer.late_render(renderable); }) {
+          renderer.late_render(renderable);
+        }
       }
     });
   });
@@ -233,7 +249,7 @@ auto show(std::index_sequence<Is...> /*seq*/, Renderables&&... renderables) {
 //------------------------------------------------------------------------------
 auto show(interactively_renderable auto&&... renderables) {
   show(std::make_index_sequence<sizeof...(renderables)>{},
-         std::forward<decltype(renderables)>(renderables)...);
+       std::forward<decltype(renderables)>(renderables)...);
 }
 //==============================================================================
 }  // namespace tatooine::rendering::interactive
