fa4380af2ec09e945a34769e651e720631144461
Steve Wolligandt <steve@isg.cs.uni-magdeburg.de>
2022-07-26 10:31:04 +0200

    -

--- examples/regular_flowmap_discretization.cpp
+++ examples/regular_flowmap_discretization.cpp
@@ -5,7 +5,7 @@
 using namespace tatooine;
 //==============================================================================
 auto main() -> int {
-  auto v   = analytical::fields::numerical::doublegyre{};
+  auto v   = analytical::numerical::doublegyre{};
   auto phi = regular_flowmap_discretization<real_number, 2>{
       flowmap(v), 0, 10, vec2{0, 0}, vec2{2, 1}, 200, 100};
   auto g = rectilinear_grid{linspace{0.0, 2.0, 1000}, linspace{0.0, 1.0, 500}};
